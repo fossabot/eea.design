d2f6030aa6992c59387cf87f43da8dd07fe5a61d
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-10-05 11:37:38 +0000

    Initial import

---
+++ eea/design/__init__.py
@@ -0,0 +1,11 @@
+from Products.CMFCore.DirectoryView import registerDirectory
+from Products.CMFCore import utils
+from Globals import package_home
+from os.path import dirname
+
+GLOBALS = globals()
+
+ppath = utils.ProductsPath
+utils.ProductsPath.append(dirname(package_home(GLOBALS)))
+registerDirectory('skins', GLOBALS)
+utils.ProductsPath = ppath
