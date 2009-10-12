59ceece9de69e3e00a0d8029548547e806dd3014
Sasha Vinčić <vincic@users.noreply.github.com>
2009-10-12 15:54:22 +0000

    remove sitemap, faq and glossary from global site actions and add icons to contact us and subscriptions #1690

--- eea/design/setuphandlers.py
+++ eea/design/setuphandlers.py
@@ -5,3 +5,13 @@ def migrate_from_eeadesign2006(context):
     portal =  context.getSite()
     css =  portal.portal_css
     css.manage_removeStylesheet('eea-highlights.css')
+
+    # remove old actions since we can't do it with GenericSetup
+    atool = getToolByName(portal, 'portal_actions')
+    actions =[ a.id for a in  atool.listActions()]
+    actionPos = []
+    for a in ['glossary', 'sitemap','faq']:
+        if a in actions:
+            actionPos.append(actions.index(a))
+    if actionPos:
+        portal.portal_actions.deleteActions(actionPos)
