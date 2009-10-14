eb916c0ba69328ab78efff3697b70c2936a01835
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-10-14 12:13:49 +0000

    Copied global_searchbox from EEADesign2006 #2483

---
+++ eea/design/skins/eea_design/global_searchbox.js
@@ -0,0 +1,25 @@
+// This script adds the description label to the search textfield.
+// We can't use jQuery here due to JS library conflicts in some
+// sites using EEATemplateService. #2455
+
+function global_searchbox_init() {
+    var form = document.getElementById('searchbox_terminology');
+    if (form == null) {
+        return
+    }
+    var term = form.term;
+    var search_label = term.title + "...";
+    term.onfocus = function() {
+        if (this.value == search_label) {
+            this.value = "";
+        }
+    };
+    term.onblur = function() {
+        if (this.value == "") {
+            this.value = search_label;
+        }
+    };
+    term.value = search_label;
+}
+
+registerPloneFunction(global_searchbox_init);
