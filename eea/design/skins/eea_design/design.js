e7660918049de04b0ae7e175334282ba9936d6c4
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-11-06 18:46:07 +0000

    Made campaign-mode look good again

--- eea/design/skins/eea_design/design.js
+++ eea/design/skins/eea_design/design.js
@@ -15,29 +15,21 @@ $(window).resize(function() {
         wrapper.width(DESIGN_MAX_WIDTH);
     }
 
-    var topNewsArea = $('#top-news-area');
-    topNewsArea.height(topNewsArea.width() * 0.1);
     // Layout of top promotions. For some reason IE got a tiny margin to the
     // right (worked if I set the .portlet-promotion width to 19%), when
     // doing this in pure CSS. Don't know why. Think it's us who are setting
     // an IE specific margin/padding somewhere, but right now I don't have the
     // time nor tools to find out where.
     // --Per Thulin, 2009-11-05
-    var margin = $('#top-news-area').width() * 0.03; // 5% margin
+    var margin = $('#top-news-area').width() * 0.01;
     var w = ($('#top-news-area').width() - 4 * margin) / 5;
     $('#top-news-area .portlet-promotions').width(w);
     $('#top-news-area .portlet-promotions:lt(4)').css('marginRight', Math.floor(margin) + 'px');
     $('#top-news-area .portlet-promotions:last').css({'marginRight': '0', 'float': 'right'});
 
-    // Multimedia highlights layout
-    //var wrapper_w = $("#multimedia-highlights").width();
-    //var big_img = $("#multimedia-highlights img:last").width(0.7 * wrapper_w);
-    //$("#multimedia-highlights ul").width(0.2 * wrapper_w).height(big_img.height());
-    //$("#multimedia-highlights ul img").width(0.2 * wrapper_w);
-
     // Make sure the height of our images stick to 16:9. Can be removed when
     // we have correct aspect ratio on the uploaded images.
-    $("#multimedia-highlights img, #top-news-area img").each(function(i) {
+    $("#multimedia-highlights img, #top-news-area .portlet-promotions img").each(function(i) {
         $(this).height((9/16) * $(this).width());
     });
 
