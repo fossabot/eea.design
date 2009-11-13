f76ad5f4819bef179fb6a2d0a346f8106ad9f212
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-11-13 14:36:39 +0000

    Display info/warning dialog before printing #2791

---
+++ eea/design/skins/eea_design/print_warning.js
@@ -0,0 +1,27 @@
+/**
+ * This JS displays a information/warning dialog about the environmental
+ * affects of printing when pressing the print button either in document
+ * actions or in IE. It looks for a #print-warning div in order to find the
+ * actual text.
+ *
+ * #2791
+ */
+
+var warning_displayed = false;
+
+window.onbeforeprint = function() {
+    var warning_text = $.trim($("#print-warning p").html());
+    if (warning_displayed == false) {
+        alert(warning_text);
+    }
+};
+
+$(document).ready(function() {
+    $('#icon-print').parent().attr('href', '#').click(function() {
+        var warning_text = $.trim($("#print-warning p").html());
+        if (confirm(warning_text)) {
+            warning_displayed = true;
+            window.print();
+        }
+    });
+});
