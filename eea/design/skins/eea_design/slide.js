cce6347767ef542d372f29e54eb142fe8c5d4c0e
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2009-12-10 14:06:43 +0000

    * Map tooltip not shown where there is already an events map on the page. [ghicaale #2771]

--- eea/design/skins/eea_design/slide.js
+++ eea/design/skins/eea_design/slide.js
@@ -1,21 +1,15 @@
 $(document).ready(function() {
-	
         $("#portal-siteactions a").each(function(i) {
-		var a = $(this);
-		a.attr("title","").attr("href", "#");
+                var a = $(this);
+                a.attr("title","").attr("href", "#");
 
-		// the tooltip panel should have the id in form of 
+                // the tooltip panel should have the id in form of
                 // tip-SITEACTION-ID
-		a.tooltip({
-			tip: '#tip-' + a.parent().attr('id'),
-			    position: 'bottom right',
-			    offset: [5, -100],
-			    events: {
-			    tooltip: 'mouseover'
-			}
-
-		    });
-	    });
-
-		
+                a.tooltip({
+                        tip: '#tip-' + a.parent().attr('id'),
+                        position: 'bottom right',
+                        offset: [5, -100],
+                        events: { tooltip: 'mouseover' }
+                    });
+            });
 });
