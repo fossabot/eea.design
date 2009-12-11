a099ea65b88cadf1ae0c1c6bd13794c74a8a0b0d
Sasha Vinčić <vincic@users.noreply.github.com>
2009-12-11 09:37:55 +0000

    all links in links in cross site bare are now normal links and some have drop down panels that show up if you have js enabled example content is added in events, contact us and subscription panels. ETCS sites and choose language contain same contet that was in the dropdown list before #2771

--- eea/design/skins/eea_design/slide.js
+++ eea/design/skins/eea_design/slide.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
-        $("#portal-siteactions a").each(function(i) {
-                var a = $(this);
+       function panel(i) {
+            var a = $(this);
+            if ($('#tip-' + a.parent().attr('id')).length > 0) {
                 a.attr("title","").attr("href", "#");
+
+		a.mouseover(function() {
+			a.parent().addClass("selected");
+		    });
 
                 // the tooltip panel should have the id in form of
                 // tip-SITEACTION-ID
                 a.tooltip({
-                        tip: '#tip-' + a.parent().attr('id'),
+                        tip:  '#tip-' + a.parent().attr('id'),
                         position: 'bottom right',
-                        offset: [5, -100],
-                        events: { tooltip: 'mouseover' }
-                    });
-            });
+                        offset: [0, -100],
+			events: { tooltip: 'mouseover' 
+                    },
+		    onShow: function(e) {
+			a.parent().addClass("selected");
+		    },
+		    onBeforeHide: function(e, i) {
+		        a.parent().removeClass("selected");
+		    }
+                });
+	    }
+       }
+    $("#portal-siteactions a").each(panel);
+    $("#portal-externalsites a").each(panel);
 });
