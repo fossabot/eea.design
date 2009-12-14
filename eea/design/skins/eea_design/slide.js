9e8938b77557df309dffda6f0df981b50b78f775
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-12-14 14:29:58 +0000

    Indentation

--- eea/design/skins/eea_design/slide.js
+++ eea/design/skins/eea_design/slide.js
@@ -1,30 +1,31 @@
 $(document).ready(function() {
-       function panel(i) {
-            var a = $(this);
-            if ($('#tip-' + a.parent().attr('id')).length > 0) {
-                a.attr("title","").attr("href", "#");
+    function panel(i) {
+        var a = $(this);
+        if ($('#tip-' + a.parent().attr('id')).length > 0) {
+            a.attr("title","").attr("href", "#");
 
-		a.mouseover(function() {
-			a.parent().addClass("selected");
-		    });
+            a.mouseover(function() {
+                a.parent().addClass("selected");
+            });
 
-                // the tooltip panel should have the id in form of
-                // tip-SITEACTION-ID
-                a.tooltip({
-                        tip:  '#tip-' + a.parent().attr('id'),
-                        position: 'bottom right',
-                        offset: [0, -100],
-			events: { tooltip: 'mouseover' 
-                    },
-		    onShow: function(e) {
-			a.parent().addClass("selected");
-		    },
-		    onBeforeHide: function(e, i) {
-		        a.parent().removeClass("selected");
-		    }
-                });
-	    }
-       }
+            // the tooltip panel should have the id in form of
+            // tip-SITEACTION-ID
+            a.tooltip({
+                tip: '#tip-' + a.parent().attr('id'),
+                position: 'bottom right',
+                offset: [0, -100],
+                events: {
+                    tooltip: 'mouseover' 
+                },
+                onShow: function(e) {
+                    a.parent().addClass("selected");
+                },
+                onBeforeHide: function(e, i) {
+                    a.parent().removeClass("selected");
+                }
+            });
+        }
+    }
     $("#portal-siteactions a").each(panel);
     $("#portal-externalsites a").each(panel);
 });
