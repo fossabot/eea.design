b0141c3daeade66c9938980e1c10ed1611a9dd1f
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-12-15 08:26:25 +0000

    Changed file encoding from latin1 to UTF-8. All templates should be UTF-8. This is why the contact-us panel didn't render the names correctly

--- eea/design/skins/eea_design/slide.js
+++ eea/design/skins/eea_design/slide.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
     function panel(i) {
         var a = $(this);
-        if ($('#tip-' + a.parent().attr('id')).length > 0) {
+        var tooltip = $('#tip-' + a.parent().attr('id'));
+        if (tooltip.length > 0) {
             a.attr("title","").attr("href", "#");
 
             a.mouseover(function() {
                 a.parent().addClass("selected");
             });
 
+            var offsets = {
+                'tip-siteaction-eionet': [0, 0],
+                'tip-siteaction-envirowindows': [0, 0],
+                'tip-siteaction-etcssites': [0, 0],
+                'tip-siteaction-subscriptions': [0, 0],
+                'tip-siteaction-contactus': [0, 0],
+                'tip-siteaction-chooselang': [0, 0]
+            }
+
+            console.log(tooltip.width());
+
+            console.log(tooltip.attr('id') + ' -> ' + offsets[tooltip.attr('id')]);
+
             // the tooltip panel should have the id in form of
             // tip-SITEACTION-ID
             a.tooltip({
-                tip: '#tip-' + a.parent().attr('id'),
-                position: 'bottom right',
-                offset: [0, -100],
+                tip: tooltip[0],
+                position: 'bottom center',
+                offset: offsets[tooltip.attr('id')],
                 events: {
                     tooltip: 'mouseover' 
                 },
