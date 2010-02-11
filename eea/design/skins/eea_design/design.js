20ef7d27c843a5c817baa626bbbab51b4bf11f95
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-02-11 13:34:16 +0000

    If galleryView jQuery plugin is present, we turn any <ul> with id #galleryView into a slideshow

--- eea/design/skins/eea_design/design.js
+++ eea/design/skins/eea_design/design.js
@@ -8,6 +8,17 @@ $(document).ready(function() {
         $('body').addClass('fullscreen');
         $('#icon-full_screen').parent().remove();
     }
+
+    if ($.fn.galleryView !== undefined) {
+        $('#galleryView').galleryView({
+            panel_width: 768,
+            panel_height: 511,
+            frame_width: 50,
+            frame_height: 50,
+            transition_speed: 350,
+            transition_interval: 0
+        });
+    }
 });
 
 $(window).load(function() {
