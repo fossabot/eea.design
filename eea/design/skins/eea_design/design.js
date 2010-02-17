9d2ecdbc46eacb360ec2a5dd98116cc335ae6180
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-02-17 10:16:29 +0000

    Generalized the tooltip system #3003

--- eea/design/skins/eea_design/design.js
+++ eea/design/skins/eea_design/design.js
@@ -19,6 +19,39 @@ $(document).ready(function() {
             transition_interval: 0
         });
     }
+    if ($.fn.tooltip !== undefined) {
+        $(".eea-tooltip-top").each(function(i) {
+            var title = $(this).attr("title");
+            $(this).after($('<div class="eea-tooltip-markup-top">' + title + '</div>'));
+            $(this).tooltip({
+                effect: 'slide'
+            });
+        });
+        $(".eea-tooltip-bottom").each(function(i) {
+            var title = $(this).attr("title");
+            $(this).after($('<div class="eea-tooltip-markup-bottom">' + title + '</div>'));
+            $(this).tooltip({
+                effect: 'slide',
+                position: 'bottom center'
+            });
+        });
+        $(".eea-tooltip-left").each(function(i) {
+            var title = $(this).attr("title");
+            $(this).after($('<div class="eea-tooltip-markup-left">' + title + '</div>'));
+            $(this).tooltip({
+                effect: 'slide',
+                position: 'center left'
+            });
+        });
+        $(".eea-tooltip-right").each(function(i) {
+            var title = $(this).attr("title");
+            $(this).after($('<div class="eea-tooltip-markup-right">' + title + '</div>'));
+            $(this).tooltip({
+                effect: 'slide',
+                position: 'center right'
+            });
+        });
+    }
 });
 
 $(window).load(function() {
