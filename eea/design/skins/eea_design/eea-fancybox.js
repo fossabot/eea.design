fcfbb743fd7902060ee7dda91787a77898b9f501
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-04 09:41:55 +0000

    Created CSS class for opening gallery views in fancybox

--- eea/design/skins/eea_design/eea-fancybox.js
+++ eea/design/skins/eea_design/eea-fancybox.js
@@ -1,4 +1,16 @@
 // Integration JS between fancybox and EEA
 $(document).ready(function() {
     $('.fancybox').fancybox();
+    $('.gallery-fancybox').each(function() {
+        var href = $(this).attr('href') + "/gallery_fancybox_view";
+        $(this).attr('href', href);
+        $(this).fancybox({
+            type: 'iframe',
+            padding: 0,
+            margin: 0,
+            width: 780,
+            height: 580,
+            autoDimensions: false
+        });
+    });
 });
