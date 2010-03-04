1c0f09a9ecdba96334052f7e52942db0d3359122
Alin Voinea <contact@avoinea.com>
2010-03-04 16:45:10 +0000

    #2986 Implemented flexible tooltips in order to be able to add variable length tooltips.

--- eea/design/skins/eea_design/eea-tooltips.js
+++ eea/design/skins/eea_design/eea-tooltips.js
@@ -1,6 +1,7 @@
 /* This JS integrates the jQuery Tools Tooltips with the EEA site. */
 $(document).ready(function() {
     if ($.fn.tooltip !== undefined) {
+        // Inflexible tooltips
         $(".eea-tooltip-top").each(function(i) {
             var title = $(this).attr("title");
             $(this).after($('<div class="eea-tooltip-markup-top">' + title + '</div>'));
@@ -31,6 +32,91 @@ $(document).ready(function() {
                 effect: 'slide',
                 position: 'center right'
             });
+        });
+
+        // Flexible tooltips
+        $(".eea-flexible-tooltip-right").each(function(i){
+          var title = $(this).attr("title");
+
+          var container = $('<div>').addClass('eea-tooltip-markup');
+          var bottomright = $('<div>').addClass('tooltip-box-br');
+          var topleft = $('<div>').addClass('tooltip-box-tl');
+          var content = $('<div>').addClass('tooltip-box-rcontent');
+          content.text(title);
+
+          topleft.append(content);
+          bottomright.append(topleft);
+          container.append(bottomright);
+
+          $(this).after(container);
+          $(this).tooltip({
+            effect: 'slide',
+            position: 'center right',
+            offset: [20, 20]
+          });
+        });
+
+        $(".eea-flexible-tooltip-left").each(function(i){
+          var title = $(this).attr("title");
+
+          var container = $('<div>').addClass('eea-tooltip-markup');
+          var bottomright = $('<div>').addClass('tooltip-box-br');
+          var topleft = $('<div>').addClass('tooltip-box-tl');
+          var content = $('<div>').addClass('tooltip-box-lcontent');
+          content.text(title);
+
+          topleft.append(content);
+          bottomright.append(topleft);
+          container.append(bottomright);
+
+          $(this).after(container);
+          $(this).tooltip({
+            effect: 'slide',
+            position: 'center left',
+            offset: [20, -10]
+          });
+        });
+
+        $(".eea-flexible-tooltip-top").each(function(i){
+          var title = $(this).attr("title");
+
+          var container = $('<div>').addClass('eea-tooltip-markup');
+          var bottomright = $('<div>').addClass('tooltip-box-br');
+          var topleft = $('<div>').addClass('tooltip-box-tl');
+          var content = $('<div>').addClass('tooltip-box-tcontent');
+          content.text(title);
+
+          topleft.append(content);
+          bottomright.append(topleft);
+          container.append(bottomright);
+
+          $(this).after(container);
+          $(this).tooltip({
+            effect: 'slide',
+            position: 'top center',
+            offset: [10, 0]
+          });
+        });
+
+        $(".eea-flexible-tooltip-bottom").each(function(i){
+          var title = $(this).attr("title");
+
+          var container = $('<div>').addClass('eea-tooltip-markup');
+          var bottomright = $('<div>').addClass('tooltip-box-br');
+          var topleft = $('<div>').addClass('tooltip-box-tl');
+          var content = $('<div>').addClass('tooltip-box-bcontent');
+          content.text(title);
+
+          topleft.append(content);
+          bottomright.append(topleft);
+          container.append(bottomright);
+
+          $(this).after(container);
+          $(this).tooltip({
+            effect: 'slide',
+            position: 'bottom center',
+            offset: [30, 0]
+          });
         });
     }
 });
