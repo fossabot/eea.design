13bb2e08140e29521a42bfcc1e0a02ebc9595aac
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-19 08:11:55 +0000

    Fixed so that TOC functions correctly with more than two header levels

--- eea/design/skins/eea_design/eea-toc.js
+++ eea/design/skins/eea_design/eea-toc.js
@@ -1,31 +1,31 @@
 /* The table of contents portlet finds all h1, h2, h3, h4 tags inside
- * the #region-content div. The script works with headers that have
- * <a> children.
+ * the #region-content div.
+ * - The script works with subheaders
+ * - The script works with headers with nested <a> tags
  */
 $(document).ready(function() {
     var currentList = $('#document-toc .portletItem ol');
     var hLevel = null;
+    var lists = {'root': currentList}
     $('#region-content').find('h2, h3, h4').each(function(i, el) {
-        var tagName = el.tagName.toLowerCase();
-        var newLevel = parseInt(tagName[1]);
+        var newLevel = parseInt(el.tagName[1]);
         hLevel = hLevel || newLevel;
 
         if (newLevel > hLevel) {
             hLevel = newLevel;
             var newList = $('<ol></ol>');
+            lists[newLevel] = newList;
             currentList.append(newList);
             currentList = newList;
-        }
-
-        if (newLevel < hLevel) {
+        } else if (newLevel < hLevel) {
             hLevel = newLevel;
-            currentList = currentList.parent();
+            currentList = lists[newLevel] || lists['root']
         }
 
         var hText = $(el).find('a').text() || $(el).text();
-        var hId = $(el).attr('id') || 'toc-' + i;
         var li = $('<li><a>' + hText + '</a></li>');
         currentList.append(li);
+        var hId = $(el).attr('id') || 'toc-' + i;
         li.find('a').attr('href', '#' + hId);
         $(el).attr('id', hId);
     });
