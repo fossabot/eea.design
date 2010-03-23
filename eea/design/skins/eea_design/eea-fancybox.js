af820b04371e068f9ebd9edf75dd42080462f90c
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-23 12:23:36 +0000

    Only do eea/fancybox integration if jquery.fancybox is installed

--- eea/design/skins/eea_design/eea-fancybox.js
+++ eea/design/skins/eea_design/eea-fancybox.js
@@ -1,18 +1,20 @@
 // Integration JS between fancybox and EEA
 $(document).ready(function() {
-    $('.fancybox').fancybox();
-    $('.gallery-fancybox').each(function() {
-        var href = $(this).attr('href') + "/gallery_fancybox_view";
-        $(this).attr('href', href);
-        $(this).fancybox({
-            type: 'iframe',
-            padding: 0,
-            margin: 0,
-            width: 780,
-            height: 580,
-            scrolling: 'no',
-            autoScale: false,
-            autoDimensions: false
+    if ($.fn.fancybox !== undefined) {
+        $('.fancybox').fancybox();
+        $('.gallery-fancybox').each(function() {
+            var href = $(this).attr('href') + "/gallery_fancybox_view";
+            $(this).attr('href', href);
+            $(this).fancybox({
+                type: 'iframe',
+                padding: 0,
+                margin: 0,
+                width: 780,
+                height: 580,
+                scrolling: 'no',
+                autoScale: false,
+                autoDimensions: false
+            });
         });
-    });
+    }
 });
