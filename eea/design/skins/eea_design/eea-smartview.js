371c178898d8a9ce3377c304c00ee94ef0839a55
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-08 09:56:09 +0000

    Add markup hooks for styling selected list entry #3185

--- eea/design/skins/eea_design/eea-smartview.js
+++ eea/design/skins/eea_design/eea-smartview.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $('#eea-template-switch a').click(function(e) {
         e.preventDefault();
         $('#main-macro-content').html('Loading...');
+        $('#eea-template-switch .selected').removeClass('selected');
+        $(this).parent().addClass('selected');
         $.get($(this).attr('href'), function(data) {
             $('#main-macro-content').html('').append(data);
         }, 'html');
