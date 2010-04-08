f4d295152ebe94e88ad2bf4ed0250d98826eafd2
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-08 09:32:36 +0000

    Add simple loading text while loading the new content (should probably be replaced with the same we use for faceted navigation)

--- eea/design/skins/eea_design/eea-smartview.js
+++ eea/design/skins/eea_design/eea-smartview.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#eea-template-switch a').click(function(e) {
         e.preventDefault();
+        $('#main-macro-content').html('Loading...');
         $.get($(this).attr('href'), function(data) {
             $('#main-macro-content').html('').append(data);
         }, 'html');
