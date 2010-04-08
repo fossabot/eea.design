769fe3e4a8cb77fa3a2712fdc137d99fad07776f
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-08 13:04:33 +0000

    Add cool fade effect when switching smart view

--- eea/design/skins/eea_design/eea-smartview.js
+++ eea/design/skins/eea_design/eea-smartview.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $('#smart-view-switch a').click(function(e) {
         e.preventDefault();
-        $('#smart-view-content').html('Loading...');
+        $('#smart-view-content').fadeOut('fast');
         $('#smart-view-switch .selected').removeClass('selected');
         $(this).parent().addClass('selected');
         $.get($(this).attr('href'), function(data) {
-            $('#smart-view-content').html('').append(data);
+            $('#smart-view-content').html('').append(data).fadeIn();
         }, 'html');
     });
 });
