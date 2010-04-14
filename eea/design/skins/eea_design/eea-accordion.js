97436f6551044f1ffa9ce5903aa2c9769bbb1a0e
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-14 13:02:17 +0000

    Load DC overview w/ AJAX when DC nav section is clicked #3255

--- eea/design/skins/eea_design/eea-accordion.js
+++ eea/design/skins/eea_design/eea-accordion.js
@@ -1,22 +1,41 @@
 $(document).ready(function() {
+
+    function loadDataCentreOverview() {
+        var themeName = window.location.href.split('/themes/')[1].split('/')[0];
+        var themesURL = window.location.href.split('/themes/')[0] + '/themes';
+        var themeCentreURL = themesURL + '/' + themeName; 
+        var dcFolderURL = themeCentreURL + '/dc';
+        $('#region-content').fadeOut();
+        $.get(dcFolderURL + '/dc_view_main_macro', function(data) {
+            $('#region-content').empty().append(data).fadeIn();
+            window.location.hash = 'dc';
+        }, 'html');
+    }
+
+    // Only show content of the active accordion
     var navTreeCurrentItem = $('.navTreeCurrentItem');
     var openAccordionPortlet = $('.eea-accordion-portlet:first');
-
-    // If the URL ends with 'dc', we open the data center accordion section.
-    if (/\/dc$/.test(window.location.pathname)) {
-        openAccordionPortlet = $('#portlet-navigation-tree-data-center-services');
-    } else if (navTreeCurrentItem) {
+    if (navTreeCurrentItem) {
         openAccordionPortlet = navTreeCurrentItem.parents('.eea-accordion-portlet');
     }
     openAccordionPortlet.addClass('eea-active-accordion');
     $('.eea-accordion-portlet').not('.eea-active-accordion').find('.eea-accordion-content').hide();
 
+    // Open accordion menu when clicked
     $('.eea-accordion-header').click(function() {
         var portletClicked = $(this).parent();
-        if (!portletClicked.hasClass('.eea-active-accordion')) {
-            $('.eea-active-accordion').removeClass('eea-active-accordion').find('.eea-accordion-content').slideUp();
-            portletClicked.addClass('eea-active-accordion').find('.eea-accordion-content').slideDown();
-            $('html,body').animate({scrollTop: 0}, 1000);
+        if (portletClicked.hasClass('.eea-active-accordion')) {
+            return;
+        }
+
+        $('.eea-active-accordion').removeClass('eea-active-accordion').find('.eea-accordion-content').slideUp();
+        portletClicked.addClass('eea-active-accordion').find('.eea-accordion-content').slideDown();
+        $('html,body').animate({scrollTop: 0}, 1000);
+
+        // Load DC overview with AJAX if the DC section is clicked
+        if (portletClicked.attr('id') == 'portlet-navigation-tree-data-center-services') {
+            loadDataCentreOverview();
         }
     });
+
 });
