03e27a6dd6ffdddfbcc821d2d8399b8fe8167353
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-05-11 15:42:58 +0000

    Also update URL with query string from batch navigation, #3238

--- eea/design/skins/eea_design/eea-smartview.js
+++ eea/design/skins/eea_design/eea-smartview.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
 
-    function loadContent(url) {
+    function loadContent() {
         $('#smart-view-content').html('<img src="++resource++faceted_images/ajax-loader.gif" />');
+        var url = $.param.querystring($.bbq.getState('smartView'), $.param.querystring());
         $.get(url, function(data) {
             $('#smart-view-content').html(data);
+            $('.listingBar a').each(function(i) {
+                var batchQueryString = $.param.querystring($(this).attr('href'));
+                var newUrl = $.param.querystring(location.href, batchQueryString)
+                $(this).attr('href', newUrl);
+            });
         }, 'html');
     }
 
     $('#smart-view-switch a').click(function(e) {
+        e.preventDefault();
         $('#smart-view-switch .selected').removeClass('selected');
         $(this).parent().addClass('selected');
-        e.preventDefault();
-        var smartView = $(this).attr('href');
-        loadContent(smartView);
         $.bbq.pushState({
-            'smartView': smartView
+            'smartView': $(this).attr('href')
         });
-    });
-
-    // Also handle click on any batch bar that will pop up
-    $('.listingBar a').live('click', function(e) {
-        e.preventDefault();
-        loadContent($(this).attr('href'));
+        loadContent();
     });
 
     $(window).bind('hashchange', function(e) {
