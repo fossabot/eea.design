984826a01aba6303cc29bebd76caec96374bf12b
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-05-11 09:37:39 +0000

    Make smartview state bookmarkable

--- eea/design/skins/eea_design/eea-smartview.js
+++ eea/design/skins/eea_design/eea-smartview.js
@@ -1,4 +1,30 @@
+var hash =  {}
+
+function saveHash() {
+    var s = ""
+    for (var key in hash) {
+        if (s.length) {
+            s += '&';
+        }
+        s += key + '=' + hash[key];
+    }
+    window.location.hash = s;
+}
+
+function loadHash() {
+    if (window.location.hash.length == 0) {
+        return;
+    }
+    var items = window.location.hash.substring(1).split('&');
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i].split('=');
+        hash[item[0]] = item[1];
+    }
+}
+
 $(document).ready(function() {
+
+    loadHash();
 
     function loadContent(url) {
         $('#smart-view-content').html('<img src="++resource++faceted_images/ajax-loader.gif" />');
@@ -11,7 +37,10 @@ $(document).ready(function() {
         $('#smart-view-switch .selected').removeClass('selected');
         $(this).parent().addClass('selected');
         e.preventDefault();
-        loadContent($(this).attr('href'));
+        var template = $(this).attr('href');
+        loadContent(template);
+        hash['template'] = template;
+        saveHash();
     });
 
     // Also handle click on any batch bar that will pop up
@@ -19,5 +48,9 @@ $(document).ready(function() {
         e.preventDefault();
         loadContent($(this).attr('href'));
     });
+
+    if ('template' in hash) {
+        $('#smart-view-switch a[href=' + hash['template'] + ']').click();
+    }
 
 });
