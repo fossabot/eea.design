cf4f83653b858167d7ba14e69ba1fbed961064f8
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-06-21 13:39:11 +0000

    Only run JSTOC on pages where there's a #document-toc element

--- eea/design/skins/eea_design/eea-toc.js
+++ eea/design/skins/eea_design/eea-toc.js
@@ -4,40 +4,44 @@
  * - The script works with headers with nested <a> tags
  */
 $(document).ready(function() {
-    var currentList = $('#document-toc .portletItem ol');
-    var hLevel = null;
-    var lists = {'root': currentList};
-    var queryString = $('#document-toc #queryString').html();
 
-    $('#region-content').find(queryString).each(function(i, el) {
-        var newLevel = parseInt(el.tagName.charAt(1));
-        hLevel = hLevel || newLevel;
+    $('#document-toc').each(function() {
+        var currentList = $(this).find('.portletItem ol');
+        var hLevel = null;
+        var lists = {'root': currentList};
+        var queryString = $(this).find(' #queryString').html();
 
-        if (newLevel > hLevel) {
-            hLevel = newLevel;
-            var newList = $('<ol></ol>');
-            lists[newLevel] = newList;
-            currentList.append(newList);
-            currentList = newList;
-        } else if (newLevel < hLevel) {
-            hLevel = newLevel;
-            currentList = lists[newLevel] || lists['root'];
-        }
+        $('#region-content').find(queryString).each(function(i, el) {
+            var newLevel = parseInt(el.tagName.charAt(1));
+            hLevel = hLevel || newLevel;
 
-        var hText = $(el).find('a').text() || $(el).text();
-        var li = $('<li><a>' + hText + '</a></li>');
-        var hId = $(el).attr('id') || 'toc-' + i;
-        var urlWithoutHash = location.protocol + '//' + location.host + location.pathname;
-        li.find('a').attr('href', urlWithoutHash + '#' + hId);
-        currentList.append(li);
-        $(el).attr('id', hId);
+            if (newLevel > hLevel) {
+                hLevel = newLevel;
+                var newList = $('<ol></ol>');
+                lists[newLevel] = newList;
+                currentList.append(newList);
+                currentList = newList;
+            } else if (newLevel < hLevel) {
+                hLevel = newLevel;
+                currentList = lists[newLevel] || lists['root'];
+            }
+
+            var hText = $(el).find('a').text() || $(el).text();
+            var li = $('<li><a>' + hText + '</a></li>');
+            var hId = $(el).attr('id') || 'toc-' + i;
+            var urlWithoutHash = location.protocol + '//' + location.host + location.pathname;
+            li.find('a').attr('href', urlWithoutHash + '#' + hId);
+            currentList.append(li);
+            $(el).attr('id', hId);
+        });
+
+        // The collapsable-portlet functionality should probably be moved to it's
+        // own file, but I'm thinking maybe we should merge it with eea-accordion
+        // in the future.
+        $('.collapsable-portlet .portletHeader').click(function() {
+            var portletClicked = $(this).parents('.portlet');
+            portletClicked.toggleClass('collapsed');
+        });
     });
 
-    // The collapsable-portlet functionality should probably be moved to it's
-    // own file, but I'm thinking maybe we should merge it with eea-accordion
-    // in the future.
-    $('.collapsable-portlet .portletHeader').click(function() {
-        var portletClicked = $(this).parents('.portlet');
-        portletClicked.toggleClass('collapsed');
-    });
 })
