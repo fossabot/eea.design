d7d9977f579cb50fdfd17b1cfa79915c2a856b71
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-10-07 12:16:48 +0000

    Create a JS that connects the searchbox with the faceted navigation search object. Refs #7647

---
+++ eea/design/skins/eea_design/soer_frontpage.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    if ( $('body').hasClass('section-soer') == false ) {
+        return;
+    }
+
+    $('#free-text-search input[type=submit]').click(function(e) {
+        e.preventDefault();
+        var searchTerm = $('#free-text-search input[type=text]').val();
+        var url = $('#search-url').html() + '#c1=' + searchTerm;
+        window.location.href = url;
+    });
+
+    $('#topic-search input[type=submit]').click(function(e) {
+        e.preventDefault();
+        var keywords = $('#topic-search select').val();
+        var url = $('#search-url').html() + '#c2=' + keywords;
+        window.location.href = url;
+    });
+
+    $('#geo-search input[type=submit]').click(function(e) {
+        e.preventDefault();
+        var keywords = $('#geo-search select').val();
+        var url = $('#search-url').html() + '#c3=' + keywords;
+        window.location.href = url;
+    });
+
+});
