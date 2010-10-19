df179e09ed668581bf3ff732a1456b49b1deaf35
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-10-19 09:26:43 +0000

    Topic search logic using topic->getThemes as no content seem to have any 'subject'.

--- eea/design/browser/soer_topic_search_logic.py
+++ eea/design/browser/soer_topic_search_logic.py
@@ -21,17 +21,49 @@ class SoerTopicSearch(BrowserView):
         tag = self.request.get('topic')
         return LABELS.get(tag, tag)
 
-    def getSynthesisReport(self):
+    def _getAllCountryItemsMatchingTopic(self):
         tag = self.request.get('topic')
-        return []
+        countries = getattr(self.context, 'countries', None)
+        brains = []
+        for folderBrain in countries.getFolderContents():
+            obj = folderBrain.getObject()
+            brains += obj.getFolderContents({
+                'getThemes': [tag],
+            })
+        return brains
+
+    def getSynthesisReport(self):
+        brains = self._getAllCountryItemsMatchingTopic()
+        ret = []
+        for brain in brains[:1]:
+            ret.append({
+                'url': brain.getURL(),
+                'title': brain.Title,
+                'description': brain.Description,
+            })
+        return ret
 
     def getThematicAssesments(self):
-        tag = self.request.get('topic')
-        return []
+        brains = self._getAllCountryItemsMatchingTopic()
+        ret = []
+        for brain in brains[:5]:
+            ret.append({
+                'url': brain.getURL(),
+                'title': brain.Title,
+                'description': brain.Description,
+            })
+        return ret
 
     def getGlobalMegatrends(self):
-        tag = self.request.get('topic')
-        return []
+        brains = self._getAllCountryItemsMatchingTopic()
+        ret = []
+        for brain in brains[:5]:
+            ret.append({
+                'url': brain.getURL(),
+                'title': brain.Title,
+                'description': brain.Description,
+            })
+        return ret
 
     def getCountryEnvironment(self):
         tag = self.request.get('topic')
