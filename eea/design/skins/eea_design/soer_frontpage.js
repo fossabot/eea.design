da05901470a287f38dfd2c0d9f002af174193e94
Antonio De Marinis <demarinis@eea.europa.eu>
2010-11-28 15:21:03 +0000

    disabled enter key event onsoer frontpage free text search, since it was not working, fixes #7900

--- eea/design/skins/eea_design/soer_frontpage.js
+++ eea/design/skins/eea_design/soer_frontpage.js
@@ -96,3 +96,13 @@ $(document).ready(function() {
 
 });
 
+function disableEnterKey(e)
+{
+     var key;      
+     if(window.event)
+          key = window.event.keyCode; //IE
+     else
+          key = e.which; //firefox      
+
+     return (key != 13);
+}
