549888b6423e207a5e2c161b058ab8676e229721
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2012-03-16 16:02:28 +0000

    Change: folder_listing is now throwing NotFound instead of Unauthorized for two reasons, one that Google bot or other bot will not return after it receive am NotFound (404) and second to keep the Zope logs clean. Fixes #9000

---
+++ eea/design/skins/eea_design/raiseNotFound.py
@@ -0,0 +1,11 @@
+## Script (Python) "raiseUnauthorized"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+from zExceptions import NotFound
+raise NotFound
