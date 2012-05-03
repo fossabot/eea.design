75507a5830ec6e530c7a9ea63177ccb4dd63fdbc
ichim-david <ichim.david@gmail.com>
2012-05-03 14:38:17 +0000

    Fixes #9125; fixed blurry text for ie8; added transition declaration for IE 10; use a jquery fade for ie9 and under for the ecotip images;

--- eea/design/skins/eea_design/design.js
+++ eea/design/skins/eea_design/design.js
@@ -22,29 +22,44 @@ jQuery(document).ready(function($) {
     /**
      * Function to animate ecotip bulb
     */
-    function toggleEcotipClass(){
-        var ecotip = jQuery('#portlet-ecotip');
-        ecotip.toggleClass('hover');
-    }
+    var toggleEcotipClass = function(){
+        var ecotip = jQuery('#portlet-ecotip'),
+            ie = $.browser.msie  && (parseInt($.browser.version, 10) < 10), 
+            action, bulb, led;
+        if(ie) {
+            bulb = ecotip.find('.ecotip-bulb');
+            led = ecotip.find('.led-bulb');
+            action = function(){
+                bulb.fadeToggle();
+                led.fadeToggle();
+            };
+        }
+        else {
+            action = function(){ecotip.toggleClass('hover');}; 
+        }
 
+        toggleEcotipClass = function() {
+            return action();
+        };
+        return toggleEcotipClass();
+    };
+    toggleEcotipClass();
     window.setInterval(toggleEcotipClass, 5000);
 
+    function themePromotionPortlets(top_news) {
+        var top_news_width = top_news.width();
+        var margin = top_news_width * 0.012;
+        w = Math.floor((top_news_width - 5 * margin) / 5);
+        var promotions = top_news.find('.portlet-promotions');
+        promotions.width(w);
+        var last = promotions.last();
+        promotions.not(last).css('marginRight', (Math.floor(margin) + 3) + 'px');
+        last.css({'marginRight': '0px'});
+    }
     // Layout of top promotions. It's safer to do this in JS as there was some rounding issues
     // with IE in window sizes that wasn't dividible by 5.
     var top_news = jQuery('#top-news-area');
     if (top_news.length) {
         themePromotionPortlets(top_news);
     }
-
 });
-
-function themePromotionPortlets(top_news) {
-    var top_news_width = top_news.width();
-    var margin = top_news_width * 0.012;
-    w = Math.floor((top_news_width - 5 * margin) / 5);
-    var promotions = top_news.find('.portlet-promotions');
-    promotions.width(w);
-    var last = promotions.last();
-    promotions.not(last).css('marginRight', (Math.floor(margin) + 3) + 'px');
-    last.css({'marginRight': '0px'});
-}
