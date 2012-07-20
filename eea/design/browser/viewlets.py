dbe0124f51f7ee685b7a5d9ae1093ae751826a3c
tiberich <tiberiuichim@users.noreply.github.com>
2012-07-20 11:56:16 +0000

    Refs #9331; plone 4.2 compatilibility; WIP

--- eea/design/browser/viewlets.py
+++ eea/design/browser/viewlets.py
@@ -69,10 +69,10 @@ class SearchViewlet(links.SearchViewlet):
     """
     _template = ViewPageTemplateFile('templates/links_search.pt')
 
-class NavigationViewlet(links.NavigationViewlet):
-    """A custom version of the links-navigation viewlet
-    """
-    _template = ViewPageTemplateFile('templates/links_navigation.pt')
+#class NavigationViewlet(links.NavigationViewlet):
+    #"""A custom version of the links-navigation viewlet
+    #"""
+    #_template = ViewPageTemplateFile('templates/links_navigation.pt')
 
 class LanguageSelectorViewlet(common.ViewletBase):
     """ A custom viewlet registered below the title for language selection
