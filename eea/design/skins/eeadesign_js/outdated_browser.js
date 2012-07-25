7afa2b55729e20d365c4a71a92ae0027c9fb899c
ichim-david <ichim.david@gmail.com>
2012-07-25 07:40:38 +0000

    Refs #8654; added outdated browser notification for IE < 9

---
+++ eea/design/skins/eeadesign_js/outdated_browser.js
@@ -0,0 +1,50 @@
+/*jslint browser: true,  */ /*global jQuery */
+(function() {
+    function badBrowser(){
+        if(jQuery.browser.msie && parseInt(jQuery.browser.version, 10) <= 8){ return true;}
+        return true;
+    }
+
+    function getBadBrowser(c_name)
+    {
+        var c_start, c_end;
+        if (document.cookie.length > 0)
+        {
+        c_start=document.cookie.indexOf(c_name + "=");
+        if (c_start !== -1)
+            {
+            c_start = c_start + c_name.length+1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end === -1) { c_end = document.cookie.length; }
+            return window.escape(document.cookie.substring(c_start, c_end));
+            }
+        }
+        return "";
+    }
+
+    function setBadBrowser(c_name, value, expiredays)
+    {
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + expiredays);
+        document.cookie = c_name+ "=" +window.escape(value) + ((expiredays === null) ? "" : ";expires=" + exdate.toGMTString());
+    }
+
+    if(badBrowser() && getBadBrowser('browserWarning') !== 'seen' ){
+        jQuery(function(){
+            var outdated = jQuery("#outdated_wrap"), outdated_fade, timeout;
+            outdated.prependTo("body").fadeIn(1000);
+            outdated_fade = function(){
+                if(outdated.is(':visible')) {
+                    setBadBrowser('browserWarning','seen', 2);
+                    outdated.fadeOut(1000);
+                }
+            };
+            timeout = window.setTimeout(outdated_fade, 10000);
+            outdated.hover(function(){
+                window.clearTimeout(timeout);
+            }, function(){
+                outdated_fade();
+            });
+        });
+    }
+}());
