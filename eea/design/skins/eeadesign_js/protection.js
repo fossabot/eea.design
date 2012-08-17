813c24a051130a9bd12875fdbae15c8e82bf62bc
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2012-08-17 12:49:31 +0000

    Updated svn:ignore property

--- eea/design/skins/eeadesign_js/protection.js
+++ eea/design/skins/eeadesign_js/protection.js
@@ -1,7 +1,5 @@
 function create_contact_info(theuser,thedomain) {
-
         var thecontact=(theuser + '@' + thedomain);
         thecontact='<A href="mailto:' + thecontact + '">' + 'Email' + '</a>';
         return thecontact;
-
 }
