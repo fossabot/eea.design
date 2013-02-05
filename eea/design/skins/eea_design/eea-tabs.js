46639b404373202c052b893d958a9e98a311536f
ichim-david <ichim.david@gmail.com>
2013-02-05 15:04:21 +0000

    Removed check for daviz since it's using chart-tabs in order for the relation tabs to work

--- eea/design/skins/eea_design/eea-tabs.js
+++ eea/design/skins/eea_design/eea-tabs.js
@@ -1,11 +1,12 @@
 jQuery(document).ready(function($) {
     var eea_tabs = function(){
-        if($("#whatsnew-gallery").length || $("#daviz-view").length) {
+        if($("#whatsnew-gallery").length) {
             return;
         }
-        var $eea_tabs = $(".eea-tabs"), eea_tabs_length = $eea_tabs.length, $eea_tabs_children,
+        var $eea_tabs = $(".eea-tabs"), eea_tabs_length = $eea_tabs.length,
             $eea_tabs_panels = $(".eea-tabs-panels"), i = 0;
-        if (eea_tabs_length){
+        var $eea_tab, $eea_tabs_panel, $eea_panels, $eea_tab_children;
+        if (eea_tabs_length) {
             for (i; i < eea_tabs_length; i += 1) {
                 $eea_tab = $eea_tabs.eq(i);
                 // detach tab for dom manipulation
@@ -19,22 +20,22 @@ jQuery(document).ready(function($) {
                 $eea_tab_children = $eea_tab.children();
                 var j = 0, tabs_length = $eea_tab_children.length,
                     $tab_title, tab_title_text;
-                
+
                 // the tabs need a link so we append a link if one is not found
-                for(j; j < tabs_length; j += 1) {
+                for (j; j < tabs_length; j += 1) {
                     $tab_title = $($eea_tab_children[j]);
                     // IE 7 encloses surrounding elements withing the li so we
                     // feed it p tags and convert it to li afterwards
-                    if($tab_title[0].tagName === "P") {
-                        $tab_title.replaceWith("<li>" + $tab_title.html() + "</li>");  
+                    if ($tab_title[0].tagName === "P") {
+                        $tab_title.replaceWith("<li>" + $tab_title.html() + "</li>");
                     }
-                    if(!$tab_title.find('a').length) { 
+                    if (!$tab_title.find('a').length) {
                         tab_title_text = $tab_title.text();
                         $tab_title.text("");
                         $('<a />').attr('href', '#').html(tab_title_text).appendTo($tab_title);
                     }
                 }
-                // redo children assigment since they could have been changed from
+                // redo children assignment since they could have been changed from
                 // p to li
                 $eea_tab_children = $eea_tab.children();
                 $eea_tab.tabs($eea_panels);
@@ -44,15 +45,15 @@ jQuery(document).ready(function($) {
                 // is bigger than first tab's height or make the first tab width
                 // bigger if it's bigger than any of the tabs
                 var k = 1, first_tab = $eea_tab_children[0], first_tab_height, cur_tab;
-                if(first_tab){
+                if (first_tab) {
                     first_tab_height = first_tab.clientHeight;
-                    for(k; k < tabs_length; k += 1) {
+                    for (k; k < tabs_length; k += 1) {
                         cur_tab = $eea_tab_children[k];
-                        if(cur_tab.clientHeight < first_tab_height) {
+                        if (cur_tab.clientHeight < first_tab_height) {
                             first_tab.style.maxWidth = "152px";
                         }
-                        else if(cur_tab.clientHeight > first_tab_height) {
-                            cur_tab.style.maxWidth = "125px";
+                        else if (cur_tab.clientHeight > first_tab_height) {
+                            cur_tab.style.maxWidth = "130px";
                         }
                     }
                 }
