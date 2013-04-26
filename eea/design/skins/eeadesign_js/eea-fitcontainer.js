7ed6c8f921a95c152c587d40cb5203e2e1bb24ef
ichim-david <ichim.david@gmail.com>
2013-04-26 10:11:27 +0000

    Refs #14365; provide settings for fitcontainer plugin and enable it for eea-tabs

--- eea/design/skins/eeadesign_js/eea-fitcontainer.js
+++ eea/design/skins/eeadesign_js/eea-fitcontainer.js
@@ -1,23 +1,50 @@
 (function( $ ){
 
-  $.fn.fitContainer = function( $container, count ) {
+  $.fn.fitContainer = function( $container, options ) {
+
+     this.$container = $container;
+
+     var settings = $.extend( {
+          'method'      : 'grow',
+          'incrementBy' : 5
+        }, options);
+     var self = this;
 
     return this.each(function() {
+
       if ( !this.innerHTML.split(' ').length > 1 ) {
           return;
       }
 
       var $this = $(this),
+          $container = self.$container || $this.parent(),
           initial_container_height = $container.height(),
           initial_width = $this.width();
 
-      while ( $this.parent().height() > initial_container_height ) {
-          initial_width += count || 5;
-          $this.css('min-width', initial_width);
+      switch(settings.method) {
+          case 'grow':
+              while ( $this.parent().height() > initial_container_height ) {
+                  initial_width += settings.incrementBy;
+                  $this.css('min-width', initial_width);
+              }
+              break;
+          case 'shrink':
+              while ( $this.parent().height() > initial_container_height ) {
+                  initial_width -= settings.incrementBy;
+                  $this.css('max-width', initial_width);
+              }
+              break;
       }
 
     });
 
   };
+
+  $(function(){
+      var $eea_tabs = $('.eea-tabs');
+      if ( $eea_tabs.length ) {
+          $eea_tabs.find('a').fitContainer($eea_tabs);
+      }
+  });
 })( jQuery );
 
