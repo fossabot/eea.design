a2df60b852958e2d1a5917879bd5b92131b9f980
ichim-david <ichim.david@gmail.com>
2013-06-11 11:48:29 +0000

    Commited fix customized on production regarding a potention recursion error with eea-fitcontainer.js

--- eea/design/skins/eeadesign_js/eea-fitcontainer.js
+++ eea/design/skins/eeadesign_js/eea-fitcontainer.js
@@ -1,17 +1,18 @@
 (function( $ ){
 
-  $.fn.fitContainer = function( $container, options ) {
+  $.fn.fitContainer = function ( $container, options ) {
 
      this.$container = $container;
 
      var settings = $.extend( {
           'method'            : 'grow',
           'incrementBy'       : 5,
-          'affectSingleWords' : false
+          'affectSingleWords' : false,
+          'maxWidth'          : 250
         }, options),
          self = this;
 
-    return this.each(function() {
+    return this.each( function () {
 
       if ( !settings.affectSingleWords && this.innerHTML.split(/\s/).length === 1 ) {
           return;
@@ -20,19 +21,26 @@
       var $this = $(this),
           $container = self.$container || $this.parent(),
           initial_container_height = $container.height(),
-          initial_width = $this.width();
+          initial_width = $this.width(),
+          $parent = $this.parent();
 
       switch( settings.method ) {
           case 'grow':
-              while ( $this.parent().height() > initial_container_height ) {
+              while ( $parent.height() > initial_container_height ) {
                   initial_width += settings.incrementBy;
                   $this.css('min-width', initial_width);
+                  if ( this.offsetWidth > settings.maxWidth ) {
+                      break;
+                  }
               }
               break;
           case 'shrink':
-              while ( $this.parent().height() > initial_container_height ) {
+              while ( $parent.height() > initial_container_height ) {
                   initial_width -= settings.incrementBy;
                   $this.css('max-width', initial_width);
+                  if ( this.offsetWidth > settings.maxWidth ) {
+                      break;
+                  }
               }
               break;
       }
@@ -41,7 +49,7 @@
 
   };
 
-  $(function(){
+  $(function () {
       // plugin call
       // this code needs to run after eea-tabs.js
       var $eea_tabs = $('.eea-tabs');
