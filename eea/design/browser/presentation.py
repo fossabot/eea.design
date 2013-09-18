4889d1cddec44bafb67c44fb16e7a894f2f739b6
David Batranu <david-batranu@users.noreply.github.com>
2013-09-18 10:06:02 +0000

    Fixing pylint, fixing output format (refs #16728)

--- eea/design/browser/presentation.py
+++ eea/design/browser/presentation.py
@@ -1,14 +1,23 @@
+""" Presentation
+"""
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
 from plone.app.layout.presentation.presentation import PresentationView
 
 
 class EEAPresentationView(PresentationView):
+    """ Customised presentation view
+    """
     def content(self):
+        """ Presentation slides (format body for presentation)
+            * convert h1 and h2 to h1
+            * arrange content in slides
+            * convert p to div (s5_slides.css hides p elements)
+        """
         soup = BeautifulSoup(self.body())
         out = []
         first = True
-        for i, elem in enumerate(soup.body.contents):
+        for elem in soup.body.contents:
             try:
                 if elem.name in ['h1', 'h2']:
                     if not first:
@@ -27,4 +36,4 @@ class EEAPresentationView(PresentationView):
                 continue
             out.append(elem.prettify())
         out = BeautifulSoup(''.join(out))
-        return out.prettify()
+        return out.body.decode_contents(formatter='html')
