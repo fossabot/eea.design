43ed6a9b32b733a5d6aef159a0125ce230082fae
Alin Voinea <contact@avoinea.com>
2013-09-27 11:49:53 +0000

    Refs #16802 - Add themes thumbs to PDF export cover

--- eea/design/browser/pdfview.py
+++ eea/design/browser/pdfview.py
@@ -1,6 +1,6 @@
 """ PDF View
 """
-from Products.Five.browser import BrowserView
+from zope.component import queryMultiAdapter
 from Products.NavigationManager.browser.navigation import getApplicationRoot
 from eea.converter.browser.app.pdfview import Cover as PDFCover
 
@@ -13,3 +13,18 @@ class Cover(PDFCover):
         """
         doc = getApplicationRoot(self.context)
         return doc.title_or_id()
+
+    @property
+    def themes(self):
+        """ Get object themes
+        """
+        themes = queryMultiAdapter((self.context, self.request),
+                                   name='themes-object')
+
+        for theme in themes.items():
+            theme = themes.item_to_short_dict(theme)
+            image = theme.get('image', None)
+            if not image:
+                continue
+            theme['image'] = image.replace('/image_icon', '/image_preview')
+            yield theme
