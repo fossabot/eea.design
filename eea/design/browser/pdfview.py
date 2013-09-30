54084975b6417391ae16af8ada5288597bfc155a
Alin Voinea <contact@avoinea.com>
2013-09-30 09:14:20 +0000

    Refs #16802 - add all "boxes styles" (callout, key facts and portal-messages)

--- eea/design/browser/pdfview.py
+++ eea/design/browser/pdfview.py
@@ -1,8 +1,29 @@
 """ PDF View
 """
+from zope.component.hooks import getSite
 from zope.component import queryMultiAdapter
 from Products.NavigationManager.browser.navigation import getApplicationRoot
 from eea.converter.browser.app.pdfview import Cover as PDFCover
+from eea.converter.pdf.adapters import OptionsMaker as PDFOptionsMaker
+
+class OptionsMaker(PDFOptionsMaker):
+    """ Custom PDF options maker for EEA ctypes
+    """
+    @property
+    def header(self):
+        """ Safely get pdf.header
+        """
+        if not self._header:
+            self._header = getSite().absolute_url() + '/pdf.header'
+        return self._header
+
+    @property
+    def footer(self):
+        """ Safely get pdf.footer
+        """
+        if not self._footer:
+            self._footer = getSite().absolute_url() + '/pdf.footer'
+        return self._footer
 
 class Cover(PDFCover):
     """ PDF Cover
