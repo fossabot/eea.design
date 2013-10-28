d1435d8e73d7a0bf7f5d397aa31448f8b73a5906
ichim-david <ichim.david@gmail.com>
2013-10-28 13:51:09 +0000

    Refs #16879; Changed the following:
    - Remove paragraphs from the presentation that do not have a class in order
      to avoid adding too much information
    - Added preliminary help text with the eea:daviz: Add presentation permission

--- eea/design/browser/javascripts/presentation.js
+++ eea/design/browser/javascripts/presentation.js
@@ -5,18 +5,39 @@ var slides = document.getElementsByClassName('slide');
 
 var scroll_top = function(){
   document.body.scrollTop = document.documentElement.scrollTop = 0;
-  for (var i = 0; i < slides.length; i++){
+  for (var i = 0, slides_length = slides.length; i < slides_length; i++){
     var slide = slides[i];
     slide.scrollTop = 0;
   }
 };
 
+function showHelpMessage(el) {
+    // hide or show help message if user has correct permissions and
+    // the slide doesn't have any content
+    var current_value = document.getElementById(el).value;
+    var current_slide_id = "slide" + current_value;
+    var current_slide = document.getElementById(current_slide_id);
+    var help_message = document.getElementById('presentation_help_message');
+    if ( current_slide.childNodes.length < 2 && help_message ) {
+        if ( help_message.className !== "visible" ) {
+            help_message.className = "visible";
+        }
+    }
+    else if ( help_message ) {
+        if ( help_message.className !== "hidden" ) {
+            help_message.className = "hidden";
+        }
+    }
+}
+
 go = function(step){
   original_go(step);
+  showHelpMessage("jumplist");
   scroll_top();
 };
 
 subgo = function(step){
   original_subgo(step);
+  showHelpMessage("jumplist");
   scroll_top();
 };
--- eea/design/browser/presentation.py
+++ eea/design/browser/presentation.py
@@ -1,5 +1,6 @@
 """ Presentation
 """
+from Products.CMFCore.utils import getToolByName
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
 from plone.app.layout.presentation.presentation import PresentationView
@@ -12,7 +13,8 @@ class EEAPresentationView(PresentationView):
         """ Presentation slides (format body for presentation)
             * convert h1 and h2 to h1
             * arrange content in slides
-            * filter out empty slides (have no/hidden content)
+            * convert img or iframe containing p tags to div
+            * filter out paragraphs that have no classes
         """
         soup = BeautifulSoup(self.body())
         out = []
@@ -28,6 +30,11 @@ class EEAPresentationView(PresentationView):
                     elem.clear()
                     elem.append(text)
                     first = False
+                elif elem.name == 'p' and \
+                        (elem.find('iframe') or elem.find('img')):
+                    elem.name = 'div'
+                elif elem.name == 'p' and not elem.has_attr('class'):
+                    continue
             except AttributeError:
                 pass
             if isinstance(elem, NavigableString) and not elem.strip():
@@ -41,10 +48,17 @@ class EEAPresentationView(PresentationView):
         for slide in slides:
             visible_content = []
             for elem in slide.contents:
-                name = getattr(elem, 'name', '')
-                if name not in ['', 'h1']:
+                name = getattr(elem, 'name', None)
+                if name and name not in ['h1']:
                     visible_content.append(elem)
             if visible_content:
                 html = slide.decode(formatter='html')
                 final_html.append(html)
         return ''.join(final_html)
+
+    def can_show_help_message(self):
+        """ Check permission before displaying help text within presentation
+        """
+        mtool = getToolByName(self.context, "portal_membership")
+        return mtool.checkPermission('eea.daviz: Add presentation',
+                                     self.context)
