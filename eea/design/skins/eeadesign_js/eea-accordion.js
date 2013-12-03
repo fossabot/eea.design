25dc37c01b6f6ffe6e640c77e75400d73d37aec3
ichim-david <ichim.david@gmail.com>
2013-12-03 14:22:41 +0000

    * Bug fix: no longer open multiple tabs when clicking on currently opened accordion tab [ichimdav refs #17631]

--- eea/design/skins/eeadesign_js/eea-accordion.js
+++ eea/design/skins/eeadesign_js/eea-accordion.js
@@ -3,40 +3,43 @@ jQuery(document).ready(function($) {
     // Products.NavigationManager
     var portlet = jQuery('dl.portletNavigationTree');
     if(portlet.length){
-      var tabs = jQuery('dd.portletItem', portlet);
+        var tabs = jQuery('dd.portletItem', portlet);
 
-      // Find current index
-      var index = 0;
-      tabs.each(function(idx, obj){
-        var here = jQuery(this);
-        if(jQuery('.navTreeCurrentNode', here).length > 0){
-          index = idx;
-          return false;
-        }
-      });
+        // Find current index
+        var index = 0;
+        tabs.each(function(idx, obj){
+            var here = jQuery(this);
+            if(jQuery('.navTreeCurrentNode', here).length > 0){
+                index = idx;
+                return false;
+            }
+        });
 
-      // Make accordion using jquery.tools
-      portlet.tabs(
-        "dl.portletNavigationTree dd.portletItem", {
-        tabs: "dt.portletSubMenuHeader",
-          effect: "slide",
-          initialIndex: index
-      });
+        // Make accordion using jquery.tools
+        portlet.tabs(
+            "dl.portletNavigationTree dd.portletItem", {
+                tabs: "dt.portletSubMenuHeader",
+                effect: "slide",
+                initialIndex: index
+            });
 
-      // Make current tab collapsible
-      portlet.delegate('.current, .collapsed', 'click', function() {
-        var tabs = portlet.data('tabs');
-        if (index == tabs.getIndex()) {
-          if(tabs.getCurrentTab().hasClass('current')){
-            tabs.getCurrentPane().slideUp();
-            tabs.getCurrentTab().removeClass('current').addClass('collapsed');
-          }else{
-            tabs.getCurrentPane().slideDown();
-            tabs.getCurrentTab().addClass('current').removeClass('collapsed');
-          }
-        }
-        index = tabs.getIndex();
-      });
+        // Make current tab collapsible
+        portlet.delegate('.current, .collapsed', 'click', function() {
+            var tabs = portlet.data('tabs');
+            var $this = $(this);
+            if (index === tabs.getIndex()) {
+                if(tabs.getCurrentTab().hasClass('current')){
+                    tabs.getCurrentPane().dequeue().stop().slideUp();
+                    tabs.getCurrentTab().removeClass('current').addClass('collapsed');
+                }else{
+                    $this.addClass('current')
+                        .removeClass('collapsed')
+                        .next()
+                        .slideDown();
+                }
+            }
+            index = tabs.getIndex();
+        });
 
     }
 
@@ -44,10 +47,10 @@ jQuery(document).ready(function($) {
     var eea_accordion = function() {
         var $folder_panels = $('.eea-accordion-panels');
         if($folder_panels.length) {
-         $($folder_panels).tabs(
-            ".eea-accordion-panels div.pane",
-            {tabs: '.eea-accordion-title, h2', effect: 'slide', initialIndex: 0}
-          );
+            $($folder_panels).tabs(
+                ".eea-accordion-panels div.pane",
+                {tabs: '.eea-accordion-title, h2', effect: 'slide', initialIndex: 0}
+            );
         }
     };
 
