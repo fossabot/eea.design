0a6715d428c8b1927a8d7423150467242e0d17e4
ichim-david <ichim.david@gmail.com>
2014-02-07 13:23:14 +0000

    Refs #18195; WIP going fullscreen when clicking on the tinymce editor  window, right now it happens for all tinyMCE until configuration is implemented

--- eea/design/skins/eea_tinymce_plugins/eeatogglefullscreen/editor_plugin.js
+++ eea/design/skins/eea_tinymce_plugins/eeatogglefullscreen/editor_plugin.js
@@ -1,32 +1,32 @@
-(function() {
-    function triggerClick(node){
-        if ( document.createEvent ) {
+(function () {
+
+    function triggerEvent (node, type) {
+        if (document.createEvent) {
             var evt = document.createEvent('MouseEvents');
-            evt.initEvent('click', true, false);
+            evt.initEvent(type, true, false);
             node.dispatchEvent(evt);
-        } else if( document.createEventObject ) {
-            node.fireEvent('onclick') ;
-        } else if (typeof node.onclick == 'function' ) {
-            node.onclick();
+        } else if (document.createEventObject) {
+            node.fireEvent('on' + type);
         }
     }
+
     tinymce.create("tinymce.plugins.EEAToggleFullScreenPlugin", {
-        init: function(d, e) {
+        init: function (d) {
             ed = d;
             var tinymce_container = document.getElementById('mce_fullscreen_container');
 
-            if ( tinymce_container) {
-                tinymce_container.onclick = function(e) {
+            if (tinymce_container) {
+                tinymce_container.onclick = function (e) {
                     if (e.target.id === "mce_fullscreen_parent") {
                         var fullscreen_button = document.getElementById('mce_fullscreen_fullscreen');
                         tinymce_container.onclick = null;
-                        triggerClick(fullscreen_button);
+                        triggerEvent(fullscreen_button, 'click');
                     }
                 };
             }
-
+            ed.onLoadContent.add(this.loadContent, this);
         },
-        getInfo: function() {
+        getInfo: function () {
             return {
                 longname: "EEA Toggle Fullscreen",
                 author: "Moxiecode Systems AB",
@@ -34,8 +34,20 @@
                 infourl: "http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/fullscreen",
                 version: tinymce.majorVersion + "." + tinymce.minorVersion
             }
+        },
+        loadContent: function (ed) {
+            var body = ed.getBody();
+            if (!body.location) {
+                if (!document.getElementById('mce_fullscreen_fullscreen')) {
+                    $(body).click(function () {
+                        var fullscreen_button = document.querySelector('.mce_fullscreen');
+                        if (!document.getElementById('mce_fullscreen_fullscreen')) {
+                            triggerEvent(fullscreen_button, 'click');
+                        }
+                    });
+                }
+            }
         }
-
     });
-    tinymce.PluginManager.add("eeatogglefullscreen", tinymce.plugins.EEAToggleFullScreenPlugin)
+    tinymce.PluginManager.add("eeatogglefullscreen", tinymce.plugins.EEAToggleFullScreenPlugin);
 })();
