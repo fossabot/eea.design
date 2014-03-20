1a2542e984ef4d59227acf3b4c7109a5156bfc30
Alin Voinea <contact@avoinea.com>
2014-03-20 16:33:27 +0000

    Tagged eea.design 9.4

--- eea/design/browser/pdfview.py
+++ eea/design/browser/pdfview.py
@@ -1,9 +1,7 @@
 """ PDF View
 """
 import os
-import urllib
-import urllib2
-import urlparse
+import urllib2, urlparse
 import contextlib
 import logging
 from bs4 import BeautifulSoup
@@ -120,45 +118,28 @@ class Body(PDFBody):
         for iframe in soup.find_all('iframe'):
             src = iframe.get('src')
             if u'embed-chart' in src:
-                # XXX wkhtmltopdf->= 0.12 Use svg instead of png
                 src = src.replace('embed-chart', 'embed-chart.png')
                 src = absolute_url(self.context,
                         url=src, default=src, view='embed-chart.png')
                 base = src.split('embed-chart.png')[0]
-                # src = src.replace('embed-chart', 'embed-chart.svg')
-                # src = absolute_url(self.context,
-                #         url=src, default=src, view='embed-chart.svg')
-                # base = src.split('embed-chart.svg')[0]
                 query = urlparse.parse_qs(urlparse.urlparse(src).query)
                 chart = query.get('chart')[0]
             elif u'embed-dashboard' in src:
-                # XXX wkhtmltopdf->= 0.12 Use svg instead of png
                 src = src.replace('embed-dashboard', 'embed-dashboard.png')
                 src = absolute_url(self.context,
                         url=src, default=src, view='embed-dashboard.png')
                 base = src.split('embed-dashboard.png')[0]
-                # src = src.replace('embed-dashboard', 'embed-dashboard.svg')
-                # src = absolute_url(self.context,
-                #         url=src, default=src, view='embed-dashboard.svg')
-                # base = src.split('embed-dashboard.svg')[0]
                 query = urlparse.parse_qs(urlparse.urlparse(src).query)
                 chart = query.get('dashboard')[0]
             else:
                 continue
 
+            src += '&tag:int=1&safe:int=0'
+
             if not src.startswith('http'):
                 src = os.path.join(self.context.absolute_url(), src)
             if not base.startswith('http'):
                 base = os.path.join(self.context.absolute_url(), base)
-
-            src = list(urlparse.urlparse(src))
-            query = urlparse.parse_qs(src[4])
-            query.update({
-                'tag:int': 1,
-                'safe:int': 0
-            })
-            src[4] = urllib.urlencode(query, doseq=True)
-            src = urlparse.urlunparse(src)
 
             code = ''
             try:
