6fa9621e6c5620d96fcc4b4338fb80c732590c80
ichim-david <ichim.david@gmail.com>
2014-03-31 14:44:54 +0000

    Refs #19082; Changed the following:
    - Added and removed resize event when the fullscreen action is triggered
    - Inline height is removed after we exit the fullscreen

--- eea/design/skins/eea_tinymce_plugins/eeatogglefullscreen/editor_plugin.js
+++ eea/design/skins/eea_tinymce_plugins/eeatogglefullscreen/editor_plugin.js
@@ -13,88 +13,91 @@
         }
     }
 
-    var debounce = function (func, threshold, execAsap) {
-        //http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
-        var timeout;
-        return function debounced () {
-            var obj = this;
-
-            function delayed () {
-                if (!execAsap)
-                    func.apply(obj, arguments);
-                timeout = null;
-            }
-
-            if (timeout)
-                clearTimeout(timeout);
-            else if (execAsap)
-                func.apply(obj, arguments);
-
-            timeout = setTimeout(delayed, threshold || 100);
-        };
-
-    };
 
     tinymce.create("tinymce.plugins.EEAToggleFullScreenPlugin", {
-        init: function (d) {
-            ed = d;
+        init: function (ed) {
+            var self = this;
+            self.ed = ed;
 
-//          if (ed.getParam('fullscreen_for')) {
-            ed.addCommand("mceFullScreen", function() {
-                var container =  this.container;
+            ed.addCommand("mceFullScreen", function () {
+                var container = this.container;
                 var $container = $(container);
                 if (container.className.indexOf('mceFullScreen') === -1) {
                     $container.addClass("mceFullScreen")
                         .find('.mceLayout').addClass("mceFullScreen").end()
                         .find('.mce_fullscreen').addClass("mceButtonActive");
+                    self.setHeight($container);
+                    $(window).on('resize.fullscreenSetHeight', self.debouncedSetHeight());
                 }
-                else  {
+                else {
                     $container.removeClass("mceFullScreen")
                         .find('.mceLayout').removeClass("mceFullScreen").end()
+                        .find('iframe').css('height', '').end()
                         .find('.mce_fullscreen').removeClass("mceButtonActive");
+
+                    $(window).off('resize.fullscreenSetHeight');
                 }
             });
             ed.addButton("fullscreen", {
                 title: "fullscreen.desc",
                 cmd: "mceFullScreen"
             });
-//          }
 
             ed.onLoadContent.add(this.loadContent, this);
+
+        },
+
+        debounce: function (func, threshold, execAsap) {
+            //http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+            var timeout;
+
+            var obj = this;
+            return function debounced () {
+
+                function delayed () {
+                    if (!execAsap)
+                        func.apply(obj, arguments);
+                    timeout = null;
+                }
+
+                if (timeout)
+                    clearTimeout(timeout);
+                else if (execAsap)
+                    func.apply(obj, arguments);
+
+                timeout = setTimeout(delayed, threshold || 100);
+            };
 
         },
 
         getInfo: function () {
             return {
                 longname: "EEA Toggle Fullscreen",
-                author: "Moxiecode Systems AB",
+                author: "David Ichim",
                 authorurl: "http://tinymce.moxiecode.com",
                 infourl: "http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/fullscreen",
                 version: tinymce.majorVersion + "." + tinymce.minorVersion
             }
         },
+
+        setHeight: function () {
+            $(this.ed.container).find('iframe').css({ 'height': $(window).height() - 110});
+        },
+
+        debouncedSetHeight: function () {
+            return this.debounce(this.setHeight, 200, false);
+        },
+
         loadContent: function (ed) {
             var body = ed.getBody();
-            var tinymce_container = ed.getContainer(),
-                container = tinymce_container,
-                $tinymce_container,
-                setHeight,
-                debouncedSetHeight;
+            var container = ed.getContainer();
 
-            if (tinymce_container) {
-                tinymce_container.onclick = function (e) {
+            if (container) {
+                container.onclick = function (e) {
                     if (e.target.className.indexOf('mceEditor') !== -1) {
                         ed.execCommand('mceFullScreen');
                     }
                 };
-                $tinymce_container = $(tinymce_container);
-
-                setHeight = function (evt) {
-                    $tinymce_container.find('iframe').css({ 'height': $tinymce_container.height() - 85});
-                };
-                debouncedSetHeight = debounce(setHeight, 200, false);
-
-                window.addEventListener('resize', debouncedSetHeight);
             }
             if (ed.getParam('fullscreen_for')) {
                 if (container.className.indexOf('mceFullScreen') === -1) {
