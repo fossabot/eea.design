69feeb7ca1e178f568af5075a4df794551aed62a
ichim-david <ichim.david@gmail.com>
2014-05-29 09:29:44 +0000

    Refs #19536; moved js-adoptHeight declaration after the hiding of the portletSubmenuHeader in order to get the desired height; cleaned soer keyfacts styles of important declarations and other measurements that were messing the height of the slidePortlet keyFacts messages

--- eea/design/skins/eea_design/design.js
+++ eea/design/skins/eea_design/design.js
@@ -1,15 +1,15 @@
 /*global jQuery window*/
 jQuery(document).ready(function($) {
     'use strict';
-    var ie = $.browser.msie  && parseInt($.browser.version, 10);
+    var ie = $.browser.msie && parseInt($.browser.version, 10);
 
     // #16878 move last two links of globalnav to a secundary container
     var secundary_portaltabs = $("<ul id='secundary-portaltabs'></ul>"),
-            global_nav = $('#portal-globalnav'),
-            global_nav_children = global_nav.children(),
-            last_two_nav_items = global_nav_children.slice(global_nav_children.length - 2);
-        last_two_nav_items.appendTo(secundary_portaltabs);
-        secundary_portaltabs.appendTo(global_nav);
+        global_nav = $('#portal-globalnav'),
+        global_nav_children = global_nav.children(),
+        last_two_nav_items = global_nav_children.slice(global_nav_children.length - 2);
+    last_two_nav_items.appendTo(secundary_portaltabs);
+    secundary_portaltabs.appendTo(global_nav);
 
     // 13830 add last-child class since ie < 9 doesn't know about this css3 selector
     $('.eea-tabs').find('li:last-child').addClass('last-child');
@@ -21,33 +21,35 @@ jQuery(document).ready(function($) {
         e.preventDefault();
     });
 
+
+    // #19536; hide navigation submenus if there are less than 2 of them
+    var $navigation_submenus = $(".portletSubMenuHeader");
+    if ($navigation_submenus && $navigation_submenus.length < 2) {
+        $navigation_submenus.hide();
+    }
+    // #19536; adopt height of given data target; keep declaration after the
+    // hiding of the navigation submenu from above
     $('.js-adoptHeight').each(function() {
         var $el = $(arguments[1]);
         var $target_el = $($el.data('target-element'));
         $el.css('height', $target_el.outerHeight());
 
     });
-
-    // #19536; hide navigation submenus if there are less than 2 of them
-    var $navigation_submenus = $(".portletSubMenuHeader");
-    if ($navigation_submenus && $navigation_submenus.length < 2) {
-      $navigation_submenus.hide();
-    }
-     // #17633 add eea-icon class to the plone message classes
-     $(".attention, .caution, .danger, .error, .hint, .important, .note, .tip, .warning").addClass('eea-icon');
+    // #17633 add eea-icon class to the plone message classes
+    $(".attention, .caution, .danger, .error, .hint, .important, .note, .tip, .warning").addClass('eea-icon');
 
 
     // #5454 remove background for required fields that have the red square
     $(".required:contains('■')").addClass('no-bg');
 
     // removed portal-column-two from @@usergroup-userprefs #4817
-    if( $("#portlet-prefs").length ) {
+    if ($("#portlet-prefs").length) {
         $("#portal-column-two").remove();
         $("#portal-column-content").removeClass('width-3:4').addClass('width-full');
     }
     // View in fullscreen for urls: /data-and-maps/figure and /data-and-maps/data
     var r = /data-and-maps\/(figures|data)\/?$/;
-    if ( r.test(window.location.pathname) ) {
+    if (r.test(window.location.pathname)) {
         $('body').addClass('fullscreen');
         $('#icon-full_screen').parent().remove();
     }
@@ -56,11 +58,11 @@ jQuery(document).ready(function($) {
     var edit_bar = $("#edit-bar");
     var edit_translate = function() {
         var translating = $("#content").find('form').find('.hiddenStructure').text().indexOf('Translating');
-        if ( translating !== -1 ) {
+        if (translating !== -1) {
             edit_bar.closest('#portal-column-content')[0].className = "cell width-full position-0";
         }
     };
-    if ( edit_bar ) {
+    if (edit_bar) {
         edit_translate();
     }
 
@@ -69,13 +71,13 @@ jQuery(document).ready(function($) {
     var $auto_related = $("#auto-related"),
         $prev = $auto_related.prev(),
         $dls = $auto_related.find('dl');
-    if ( $dls.length ) {
+    if ($dls.length) {
         $auto_related.detach();
-        $dls.each(function(idx, item){
+        $dls.each(function(idx, item) {
             var $item = $(item),
                 $dt = $item.find('dt');
-            $item.find('.portletItem').each(function(idx, item){
-                if(item.className.indexOf('embedded') === -1) {
+            $item.find('.portletItem').each(function(idx, item) {
+                if (item.className.indexOf('embedded') === -1) {
                     $(item).insertAfter($dt);
                 }
             });
@@ -85,15 +87,15 @@ jQuery(document).ready(function($) {
 
     /**
      * Function to animate ecotip bulb
-    */
-    var toggleEcotipClass = function(){
+     */
+    var toggleEcotipClass = function() {
         var ecotip = $('#portlet-ecotip'),
             action, bulb, led;
-        if ( ie && ie < 10 ) {
+        if (ie && ie < 10) {
             bulb = ecotip.find('.ecotip-bulb');
             led = ecotip.find('.led-bulb');
-            action = function(){
-                if ( $.fadeToggle ) {
+            action = function() {
+                if ($.fadeToggle) {
                     bulb.fadeToggle();
                     led.fadeToggle();
                 }
@@ -104,7 +106,9 @@ jQuery(document).ready(function($) {
             };
         }
         else {
-            action = function(){ecotip.toggleClass('hover');};
+            action = function() {
+                ecotip.toggleClass('hover');
+            };
         }
 
         toggleEcotipClass = function() {
@@ -125,10 +129,11 @@ jQuery(document).ready(function($) {
         promotions.not(last).css('marginRight', (Math.floor(margin) + 3) + 'px');
         last.css({'marginRight': '0px'});
     }
+
     // Layout of top promotions. It's safer to do this in JS as there was some rounding issues
     // with IE in window sizes that wasn't dividible by 5.
     var top_news = $('#top-news-area');
-    if ( top_news.length ) {
+    if (top_news.length) {
         themePromotionPortlets(top_news);
     }
 });
