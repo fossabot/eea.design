54a5da3e78953508bba3b9d0e79aaa100cf0d114
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2014-08-05 10:07:38 +0000

    * Feature: blocked collective.deletepermission profile not to be installed   (by mistake) via site setup   [ghicaale refs #17478]

--- eea/design/skins/eeadesign_js/block_ldapuserfolder_profile.js
+++ eea/design/skins/eeadesign_js/block_ldapuserfolder_profile.js
@@ -1,11 +1,20 @@
-// This code is ment to disable install profile of Products.LDAPUserFolder via site setup
 jQuery(document).ready(function ($) {
+    // This code is ment to disable install profile of Products.LDAPUserFolder via site setup
     var LDAPUserFolder_control = $(".configlets li input#LDAPUserFolder");
 
     if (LDAPUserFolder_control) {
         var LDAPUserFolder_configlet = LDAPUserFolder_control.parent();
 
         LDAPUserFolder_control.attr("disabled", true);
-        LDAPUserFolder_configlet.append('<label style="color: red; padding-left: 4em">Profile for LDAP User Folder was disabled as it is meant for pure CMF sites only.</label>');
+        LDAPUserFolder_configlet.append('<label style="color: red; padding-left: 4em; font-weight: normal">Profile for LDAP User Folder was disabled as it is meant for pure CMF sites only.</label>');
+    }
+
+    // This code is ment to disable install profile of collective.deletepermission via site setup
+    var deletepermission_control = $(".configlets li input#collective\\.deletepermission");
+    if (deletepermission_control) {
+        var deletepermission_configlet = deletepermission_control.parent();
+
+        deletepermission_control.attr("disabled", true);
+        deletepermission_configlet.append('<br /><label style="color: red; padding-left: 4em; font-weight: normal">Profile of collective.deletepermission should not be installed, please use the EEA associated profile.</label>');
     }
 });
