e395986c33c84d40e836910b705efc16b455fc2c
ichim-david <ichim.david@gmail.com>
2014-08-13 14:50:16 +0000

    Refs #20302; added portlet_restore_form_values portlet with initial data that displays saved form entry

--- eea/design/skins/eeadesign_js/design.js
+++ eea/design/skins/eeadesign_js/design.js
@@ -53,23 +53,61 @@ jQuery(document).ready(function($) {
     catch(err) {
         // console.log(err);
     }
-    // #20302; save state on submit attempt
-    //    var edit_form = $("form[name='edit_form']");
-    //    if (edit_form) {
-    //        (function() {
-    //            var options = {
-    //                        objName: window.location.href,
-    //                        clearOnSubmit: false
-    //                    };
-    //            edit_form.submit(function(){
-    //                var $this = $(this);
-    //                if ($this.rememberState) {
-    //                    $this.rememberState(options);
-    //                }
-    //            });
-    //        }());
-    //    }
+//    #20302; save state on submit attempt
+        var url_path_name = window.location.pathname;
+        window.EEA = window.EEA || {};
+        window.EEA.storage_utils = {};
+        var storage_utils = window.EEA.storage_utils;
+        storage_utils.getLocalStorageEntry = function(key) {
+            var storage_key = window.localStorage.key(key);
+            if (storage_key  !== key) {
+                return null;
+            }
+            return storage_key ? window.localStorage.getItem(storage_key) : null;
+        };
 
+        storage_utils.getLocalStorageEntryValue = function(storage, value) {
+            var values = JSON.parse(storage);
+            var match, current, i, length;
+            for (i = 0, length = values.length; i < length; i += 1) {
+                current = values[i];
+                if (current.name === value) {
+                    match = current.value;
+                    break;
+                }
+            }
+            return match;
+        };
+
+        var edit_form = $("form[name='edit_form']");
+        if (edit_form) {
+            (function() {
+                var options = {
+                            objName: url_path_name,
+                            clearOnSubmit: false,
+                            onSaveCallback: function(values) {
+                                values.push({
+                                    name: 'saveDate',
+                                    value: new Date().toString()
+                                });
+                                return values;
+                            }
+                        };
+                edit_form.submit(function(){
+                    var $this = $(this);
+                    if ($this.rememberState) {
+                        $this.rememberState(options);
+                    }
+                });
+
+                var edit_form_data = storage_utils.getLocalStorageEntry(url_path_name);
+                var portlet_restore = $("#js-portletRestoreForm");
+                if (edit_form_data) {
+                    portlet_restore.find('.portletRestoreForm-entry').html(storage_utils.getLocalStorageEntryValue(edit_form_data, 'saveDate'))
+                                .end().removeClass('visualHidden');
+                }
+            }());
+        }
 
     // #5454 remove background for required fields that have the red square
     $(".required:contains('■')").addClass('no-bg');
@@ -81,7 +119,7 @@ jQuery(document).ready(function($) {
     }
     // View in fullscreen for urls: /data-and-maps/figure and /data-and-maps/data
     var r = /data-and-maps\/(figures|data)\/?$/;
-    if (r.test(window.location.pathname)) {
+    if (r.test(url_path_name)) {
         $('body').addClass('fullscreen');
         $('#icon-full_screen').parent().remove();
     }
