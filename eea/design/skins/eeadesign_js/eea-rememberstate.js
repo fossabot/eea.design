6fad72e16fb5a941697db1afdf6786e9a6434c0c
ichim-david <ichim.david@gmail.com>
2014-08-18 14:31:00 +0000

    Refs #20302; bumped eea.design profile to 10.3 and moved logic of remember plugin from eea.design to eea-rememberstate.js where the logic can be enabled just for logged in users

---
+++ eea/design/skins/eeadesign_js/eea-rememberstate.js
@@ -0,0 +1,66 @@
+jQuery(document).ready(function($) {
+    'use strict';
+//    #20302; save state on submit attempt and remove it on success
+    var url_path_name = window.location.pathname;
+    window.EEA = window.EEA || {};
+    window.EEA.storage_utils = {};
+    var storage_utils = window.EEA.storage_utils;
+    storage_utils.getLocalStorageEntry = function(key) {
+        var storage_key = window.localStorage.key(key);
+        if (storage_key  !== key) {
+            return null;
+        }
+        return storage_key ? window.localStorage.getItem(storage_key) : null;
+    };
+
+    storage_utils.delLocalStorageEntry = function(key) {
+        var storage_key = window.localStorage.key(key);
+        if (storage_key  !== key) {
+            return null;
+        }
+        return storage_key ? window.localStorage.removeItem(storage_key) : null;
+    };
+
+    storage_utils.getLocalStorageEntryValue = function(storage, value) {
+        var values = JSON.parse(storage);
+        var match, current, i, length;
+        for (i = 0, length = values.length; i < length; i += 1) {
+            current = values[i];
+            if (current.name === value) {
+                match = current.value;
+                break;
+            }
+        }
+        return match;
+    };
+
+    var edit_form = $("form[name='edit_form']");
+    if (edit_form) {
+        (function() {
+            var options = {
+                objName: url_path_name,
+                clearOnSubmit: false,
+                onSaveCallback: function(values) {
+                    values.push({
+                        name: 'saveDate',
+                        value: new Date().toString()
+                    });
+                    return values;
+                }
+            };
+            edit_form.submit(function(){
+                var $this = $(this);
+                if ($this.rememberState) {
+                    $this.rememberState(options);
+                }
+            });
+
+            var edit_form_data = storage_utils.getLocalStorageEntry(url_path_name);
+            var portlet_restore = $("#js-portletRestoreForm");
+            if (edit_form_data) {
+                portlet_restore.find('.portletRestoreForm-entry').html(storage_utils.getLocalStorageEntryValue(edit_form_data, 'saveDate'))
+                    .end().removeClass('visualHidden');
+            }
+        }());
+    }
+});
