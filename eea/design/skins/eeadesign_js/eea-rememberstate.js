888d714e1eff309da71a4e9360f6881621d392e6
ichim-david <ichim.david@gmail.com>
2014-08-19 15:12:15 +0000

    Refs #20302; Changed the following:
    - Renamed portlet_restore_form_values to restore_form_values and moved it to eeadesign_templates
      since the content will no longer load within a portlet
    - Added dialog with options to choose whether the form should be restored or not

--- eea/design/skins/eeadesign_js/eea-rememberstate.js
+++ eea/design/skins/eeadesign_js/eea-rememberstate.js
@@ -2,23 +2,25 @@ jQuery(document).ready(function($) {
     'use strict';
 //    #20302; save state on submit attempt and remove it on success
     var url_path_name = window.location.pathname;
-    var saved_search_path = window.location.search.indexOf("Changes%20saved");
+    var search_path = window.location.search;
+    var saved_search_path = search_path.indexOf("Changes%20saved");
+    var timeout_search_path = search_path.indexOf('timeout=True');
     window.EEA = window.EEA || {};
     window.EEA.storage_utils = {};
     var storage_utils = window.EEA.storage_utils;
     storage_utils.getLocalStorageEntry = function(key) {
         var storage_key = window.localStorage.key(key);
-        if (storage_key  !== key) {
-            return null;
-        }
+//        if (storage_key  !== key) {
+//            return null;
+//        }
         return storage_key ? window.localStorage.getItem(storage_key) : null;
     };
 
     storage_utils.delLocalStorageEntry = function(key) {
         var storage_key = window.localStorage.key(key);
-        if (storage_key  !== key) {
-            return null;
-        }
+//        if (storage_key  !== key) {
+//            return null;
+//        }
         return storage_key ? window.localStorage.removeItem(storage_key) : null;
     };
 
@@ -57,16 +59,34 @@ jQuery(document).ready(function($) {
             });
 
             var edit_form_data = storage_utils.getLocalStorageEntry(url_path_name);
-            var portlet_restore = $("#js-portletRestoreForm");
             if (edit_form_data) {
-                portlet_restore.find('.portletRestoreForm-entry').html(storage_utils.getLocalStorageEntryValue(edit_form_data, 'saveDate'))
-                    .end().removeClass('visualHidden');
+                if (timeout_search_path !== -1) {
+                    (function(){
+                        $.get('/restore_form_values')
+                            .done(function( data ) {
+                                var portlet_restore = $(data);
+                                portlet_restore.dialog({
+                                    buttons: {
+                                        'Yes': function() {
+                                            edit_form.data("rememberState", {"objName": url_path_name, "$el": edit_form});
+                                            edit_form.rememberState('restoreState');
+                                            $(this).dialog('close');
+                                        },
+                                        'No': function() {
+                                            $(this).dialog('close');
+                                            storage_utils.delLocalStorageEntry(url_path_name + '/edit');
+                                        }
+                                    }
+                                });
+                            });
+                    }());
+                }
             }
         }());
     }
 
     // remove form state on successful form submission
     if (saved_search_path) {
-        storage_utils.delLocalStorageEntry(url_path_name);
+        storage_utils.delLocalStorageEntry(url_path_name + '/edit');
     }
 });
