53bd257709fdbee0ba1cf87c324bf4fc40bfda49
ichim-david <ichim.david@gmail.com>
2014-09-09 15:05:19 +0000

    Refs #6288; added button update-stats in order to update the readability checking of given text input

--- eea/design/skins/eeadesign_js/design.js
+++ eea/design/skins/eeadesign_js/design.js
@@ -185,25 +185,45 @@ jQuery(document).ready(function($) {
     if (edit_bar) {
         (function(){
           var $text = $("#text");
+          var $smog_index = $("#smog_index");
+          var $flesch_reading_ease = $("#flesch_kincaid_reading_ease");
+          var $kincaid_grade_level = $("#flesch_kincaid_grade_level");
           var $text_portlet = $("#stats");
+          var $average_grade_level = $("#average_grade_level");
+          var $gunning_score = $("#gunning_fog_score");
+          var $coleman_index = $("#coleman_liau_index");
+          var $automated_index = $("#automated_readability_index");
+          var $syllable_count = $("#syllable_count");
+          var $word_count = $("#word_count");
+          var $sentence_count = $("#sentence_count");
+          var $words_per_sentance = $("#words_per_sentence");
+          var $syllable_per_word = $("#syllables_per_word");
+          var $letters_per_word = $("#letters_per_word");
+
           if ($text_portlet.length < 1) {
               return;
           }
           var text_count_obj = window.textstatistics($text.html());
-          $("#smog_index").html(text_count_obj.smogIndex());
-          $("#flesch_kincaid_reading_ease").html(text_count_obj.fleschKincaidReadingEase());
-          var $kincaid_grade_level = $("#flesch_kincaid_grade_level");
-          $kincaid_grade_level.html(text_count_obj.fleschKincaidGradeLevel());
-          $("#average_grade_level").html(Math.round($kincaid_grade_level.html()));
-          $("#gunning_fog_score").html(text_count_obj.gunningFogScore());
-          $("#coleman_liau_index").html(text_count_obj.colemanLiauIndex());
-          $("#automated_readability_index").html(text_count_obj.automatedReadabilityIndex());
-          $("#syllable_count").html(text_count_obj.syllableCount(text_count_obj.text));
-          $("#word_count").html(text_count_obj.wordCount());
-          $("#sentence_count").html(text_count_obj.sentenceCount());
-          $("#words_per_sentence").html(text_count_obj.averageWordsPerSentence().toFixed(1));
-          $("#syllables_per_word").html(text_count_obj.averageSyllablesPerWord(text_count_obj.text).toFixed(1));
-          $("#letters_per_word").html(text_count_obj.averageCharactersPerWord(text_count_obj.text).toFixed(1));
+            $("#update-stats").click(function(ev){
+                set_stats();
+                ev.preventDefault();
+            });
+          var set_stats = function() {
+              $smog_index.html(text_count_obj.smogIndex());
+              $flesch_reading_ease.html(text_count_obj.fleschKincaidReadingEase());
+              $kincaid_grade_level.html(text_count_obj.fleschKincaidGradeLevel());
+              $average_grade_level.html(Math.round($kincaid_grade_level.html()));
+              $gunning_score.html(text_count_obj.gunningFogScore());
+              $coleman_index.html(text_count_obj.colemanLiauIndex());
+              $automated_index.html(text_count_obj.automatedReadabilityIndex());
+              $syllable_count.html(text_count_obj.syllableCount(text_count_obj.text));
+              $word_count.html(text_count_obj.wordCount());
+              $sentence_count.html(text_count_obj.sentenceCount());
+              $words_per_sentance.html(text_count_obj.averageWordsPerSentence().toFixed(1));
+              $syllable_per_word.html(text_count_obj.averageSyllablesPerWord(text_count_obj.text).toFixed(1));
+              $letters_per_word.html(text_count_obj.averageCharactersPerWord(text_count_obj.text).toFixed(1));
+          }
+          set_stats();
         }());
     }
 
