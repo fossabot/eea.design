f080040cca5e2f32845af82815bbab00e0547e58
Alin Voinea <contact@avoinea.com>
2014-10-08 13:53:11 +0000

    Refs #21254 - Fix pylint warnings

--- eea/design/browser/history.py
+++ eea/design/browser/history.py
@@ -11,6 +11,8 @@ class HistoryView(ContentHistoryView):
     """ Patch ContentHistoryView in order to speed it up
     """
     def revisionHistory(self):
+        """ Patched revision history
+        """
         context = aq_inner(self.context)
         if not _checkPermission(AccessPreviousVersions, context):
             return []
@@ -20,36 +22,45 @@ class HistoryView(ContentHistoryView):
             return []
 
         context_url = context.absolute_url()
-        history=rt.getHistoryMetadata(context);
+        history = rt.getHistoryMetadata(context)
         portal_diff = getToolByName(context, "portal_diff", None)
         can_diff = portal_diff is not None \
             and len(portal_diff.getDiffForPortalType(context.portal_type)) > 0
-        can_revert = _checkPermission('CMFEditions: Revert to previous versions', context)
+        can_revert = _checkPermission(
+            'CMFEditions: Revert to previous versions', context)
 
         def morphVersionDataToHistoryFormat(vdata, version_id):
+            """ Map version to history format
+            """
             meta = vdata["metadata"]["sys_metadata"]
             userid = meta["principal"]
-            info=dict(type='versioning',
-                      action=_(u"Edited"),
-                      transition_title=_(u"Edited"),
-                      actorid=userid,
-                      time=meta["timestamp"],
-                      comments=meta['comment'],
-                      version_id=version_id,
-                      preview_url="%s/versions_history_form?version_id=%s#version_preview" %
-                                  (context_url, version_id),
-                      )
+            info = dict(
+                type='versioning',
+                action=_(u"Edited"),
+                transition_title=_(u"Edited"),
+                actorid=userid,
+                time=meta["timestamp"],
+                comments=meta['comment'],
+                version_id=version_id,
+                preview_url=(
+                    "%s/versions_history_form?version_id=%s#version_preview" %
+                    (context_url, version_id)
+                )
+            )
+
             if can_diff:
-                if version_id>0:
-                    info["diff_previous_url"]=("%s/@@history?one=%s&two=%s" %
+                if version_id > 0:
+                    info["diff_previous_url"] = ("%s/@@history?one=%s&two=%s" %
                             (context_url, version_id, version_id-1))
 
-                info["diff_current_url"]=("%s/@@history?one=current&two=%s" %
-                                              (context_url, version_id))
+                info["diff_current_url"] = ("%s/@@history?one=current&two=%s" %
+                                            (context_url, version_id))
+
             if can_revert:
-                info["revert_url"]="%s/revertversion" % context_url
+                info["revert_url"] = "%s/revertversion" % context_url
             else:
-                info["revert_url"]=None
+                info["revert_url"] = None
+
             info.update(self.getUserInfo(userid))
             return info
 
