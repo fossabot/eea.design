4476e03ded02686e5710593e9c23208ad44ce938
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2017-11-24 16:30:08 +0000

    add some errors to th raven ignore list

--- eea/design/skins/eeadesign_js/sentry.js
+++ eea/design/skins/eeadesign_js/sentry.js
@@ -1,6 +1,14 @@
 $(function(){
     var sentry_dsn = $('body').data('sentry-dsn');
-    if (sentry_dsn != null){
-        Raven.config(sentry_dsn).install();
+    if (sentry_dsn !== null){
+        Raven.config(sentry_dsn, {
+            ignoreErrors: [
+                'jQuery is not defined',
+                 '$ is not defined',
+                 'Can\'t find variable: jQuery',
+                 'Socialite is not defined',
+                 'Persistent storage maximum size reached'
+            ];
+        }).install();
     }
-})
+});
