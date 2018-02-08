24118b3b9d4da6db3656777f5b395da4cb4a260b
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-02-08 11:13:42 +0100

    Refs #92742 Quickfix so that the pdf viewer is not loaded for other file types except pdf.

---
+++ eea/design/skins/eeadesign_scripts/check_pdf.py
@@ -0,0 +1,22 @@
+## Script (Python) "subscriber_account"
+##bind container=container
+##bind context=context
+##bind script=script
+##bind subpath=traverse_subpath
+##title=View pdf in browser, used in pdf viewer
+##
+
+if traverse_subpath:
+    field = context.getWrappedField(traverse_subpath[0])
+else:
+    field = context.getPrimaryField()
+if not field.checkPermission('r', context):
+    from zExceptions import Unauthorized
+    raise Unauthorized('Field %s requires %s permission' % (field, field.read_permission))
+
+file = context.getFile()
+filename = context.getFilename()
+
+if file.getContentType() not in ['application/pdf']:
+    return 0
+return 1
