d660d9453e153994feff871a8e528f66f029560a
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-02-08 13:06:17 +0100

    Refs #92742 Don't load the pdf viewer for files with mimetypes different than the one for pdfs.

---
+++ eea/design/skins/eeadesign_scripts/check_pdf.py
@@ -0,0 +1,13 @@
+## Script (Python) "subscriber_account"
+##bind container=container
+##bind context=context
+##bind script=script
+##bind subpath=traverse_subpath
+##title=Condition for checking if the file is a pdf before loading the viewer
+##
+file = context.getFile()
+filename = context.getFilename()
+
+if file.getContentType() not in ['application/pdf']:
+    return 0
+return 1
