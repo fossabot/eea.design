59b7765082ef4dea2f64aaf105477ef30aa7fba2
ichim-david <ichim.david@gmail.com>
2018-02-27 15:35:13 +0200

    Refs #91577 fixed lazy_load.js faceted call

--- eea/design/skins/eeadesign_js/lazy_load.js
+++ eea/design/skins/eeadesign_js/lazy_load.js
@@ -24,65 +24,6 @@ function enableLazy(element) {
     element.attr('src', '/www/lazyload_loader.gif');
 }
 
-// Faceted Lazy Load
-if (window.Faceted) {
-	Faceted.Events.LAZY_LOAD = 'FACETED-LAZY-LOAD';
-	Faceted.LoadLazy = {
-	    initialize: function () {
-		if(jQuery('#faceted-results').length) {
-		    var loaded_once = false;
-		    jQuery(Faceted.Events).bind(Faceted.Events.LAZY_LOAD, function(evt, data){
-			var children = jQuery('#faceted-results').children();
-			if (children.length > 1) {
-			    var lazy_elements = children.find('.lazy');
-
-			    jQuery(lazy_elements).each(function(){
-				var element = jQuery(this);
-				var source = element.attr('src');
-
-				if (source.indexOf('lazyload_loader') === -1) {
-				    element.attr('data-src', source);
-				    element.attr('src', '/www/lazyload_loader.gif');
-				    loaded_once = true;
-				}
-				else {
-				    loaded_once = false;
-				}
-			    });
-
-			    if (loaded_once) {
-				jQuery(lazy_elements).parent().css('text-align', 'center');
-
-				var windowWidth = jQuery(window).width();
-				if (windowWidth <= 767 || windowWidth > 930) {
-				    jQuery(lazy_elements).parent().css('width', '15%');
-				}
-
-				if (windowWidth <= 480 || (windowWidth > 767 && windowWidth <= 930)) {
-				    jQuery(lazy_elements).parent().css('width', '20%');
-				}
-
-				jQuery(lazy_elements).lazy({
-				    scrollDirection: 'both',
-				    effect: 'fadeIn',
-				    effectTime: 1000,
-				    threshold: 100,
-				    combined: true,
-				    delay: 3000,
-				    visibleOnly: true,
-				    onError: function(element) {
-					console.log('error loading ' + element.data('src'));
-				    }
-				});
-			    }
-			}
-		    });
-		}
-	    }
-	};
-
-}
-
 function cleanupFacetedLazy() {
     if (Faceted.Events.LAZY_LOAD && jQuery('#faceted-results').length === 0) {
         jQuery(Faceted.Events).unbind(Faceted.Events.LAZY_LOAD);
@@ -91,7 +32,62 @@ function cleanupFacetedLazy() {
 
 jQuery(document).ready(function($) {
     // Check if the faceted event needs to be cleaned up
+    // Faceted Lazy Load
     if (window.Faceted) {
+        Faceted.Events.LAZY_LOAD = 'FACETED-LAZY-LOAD';
+        Faceted.LoadLazy = {
+            initialize: function () {
+                if(jQuery('#faceted-results').length) {
+                    var loaded_once = false;
+                    jQuery(Faceted.Events).bind(Faceted.Events.LAZY_LOAD, function(evt, data){
+                        var children = jQuery('#faceted-results').children();
+                        if (children.length > 1) {
+                            var lazy_elements = children.find('.lazy');
+
+                            jQuery(lazy_elements).each(function(){
+                                var element = jQuery(this);
+                                var source = element.attr('src');
+
+                                if (source.indexOf('lazyload_loader') === -1) {
+                                    element.attr('data-src', source);
+                                    element.attr('src', '/www/lazyload_loader.gif');
+                                    loaded_once = true;
+                                }
+                                else {
+                                    loaded_once = false;
+                                }
+                            });
+
+                            if (loaded_once) {
+                                jQuery(lazy_elements).parent().css('text-align', 'center');
+
+                                var windowWidth = jQuery(window).width();
+                                if (windowWidth <= 767 || windowWidth > 930) {
+                                    jQuery(lazy_elements).parent().css('width', '15%');
+                                }
+
+                                if (windowWidth <= 480 || (windowWidth > 767 && windowWidth <= 930)) {
+                                    jQuery(lazy_elements).parent().css('width', '20%');
+                                }
+
+                                jQuery(lazy_elements).lazy({
+                                    scrollDirection: 'both',
+                                    effect: 'fadeIn',
+                                    effectTime: 1000,
+                                    threshold: 100,
+                                    combined: true,
+                                    delay: 3000,
+                                    visibleOnly: true,
+                                    onError: function(element) {
+                                        console.log('error loading ' + element.data('src'));
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            }
+        };
         cleanupFacetedLazy();
     }
 
@@ -120,7 +116,7 @@ jQuery(document).ready(function($) {
 
     if(jQuery('#faceted-results').length) {
         jQuery('#faceted-results').bind('DOMSubtreeModified',function(event) {
-    	   Faceted.LoadLazy.initialize();
+            Faceted.LoadLazy.initialize();
         });
     }
 
@@ -187,7 +183,6 @@ jQuery(document).ready(function($) {
                 if (jQuery(elem).hasClass('not_visible')){
                     jQuery(elem).removeClass('not_visible');
                     if (jQuery(elem).hasClass('isChart')){
-                        var $elem = jQuery(elem);
                         var vhash = elem.id.split('_')[2];
                         eval('gl_charts[\'googlechart_view_' + vhash + '\'] = window.drawChart(jQuery(elem).data(\'settings\'), jQuery(elem).data(\'other_options\')).chart;');
                     }
