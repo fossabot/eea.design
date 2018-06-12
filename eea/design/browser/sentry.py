a79854a408e61f928000daba3e511e09088ad045
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-06-12 16:17:23 +0300

    Refs #95646 - Get sentry environment from rancher-metadata

--- eea/design/browser/sentry.py
+++ eea/design/browser/sentry.py
@@ -1,28 +1,38 @@
 """ Sentry
 """
 import os
-from Products.Five import BrowserView
 import logging
+from eventlet.green import urllib2
+from contextlib import closing
+from Products.Five import BrowserView
+from eea.cache import cache
 
 logger = logging.getLogger("eea.design")
 
+RANCHER_METADATA = 'http://rancher-metadata/latest'
+TIMEOUT = 15
 
 class SentryDSN(BrowserView):
     """ return sentry DSN env variable
     """
+    _environment = None
+
+    @cache(lambda *args: "environment", lifetime=86400)
     def environment(self):
         """ Sentry environment
         """
-        host = self.request._orig_env.get("HTTP_X_FORWARDED_HOST")
-        if not host:
-            return "devel"
-        if host in ("www.eea.europa.eu", "eea.europa.eu"):
-            return "production"
-        elif host in ("staging.eea.europa.eu"):
-            return "staging"
-        else:
-            logger.info("Unexpected hostname, default sentry env: devel")
-            return "devel"
+        if not self._environment:
+            self._environment = os.environ.get('ENVIRONMENT',
+                                os.environ.get('SENTRY_ENVIRONMENT', ''))
+            if not self._environment:
+                url = RANCHER_METADATA + '/self/stack/environment_name'
+                try:
+                    with closing(urllib2.urlopen(url, timeout=TIMEOUT)) as con:
+                        self._environment = con.read()
+                except Exception as err:
+                    logger.exception(err)
+                    self._environment = 'devel'
+        return self._environment
 
     def version(self):
         """ KGS version
