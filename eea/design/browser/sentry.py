2ab12cbb0845acdd23f0f8d991644db73db18c69
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-06-12 10:48:18 +0300

    Refs #95646 - Send JS tracebacks to own sentry

--- eea/design/browser/sentry.py
+++ eea/design/browser/sentry.py
@@ -1,28 +1,33 @@
+""" Sentry
+"""
 import os
 from Products.Five import BrowserView
 import logging
 
-log = logging.getLogger('SENTRY_DEBUG')
+logger = logging.getLogger("eea.design")
 
 
 class SentryDSN(BrowserView):
-    """ return sentry DSN env variable """
+    """ return sentry DSN env variable
+    """
+    def environment(self):
+        """ Sentry environment
+        """
+        host = self.request._orig_env.get("HTTP_X_FORWARDED_HOST")
+        if not host:
+            return "devel"
+        if host in ("www.eea.europa.eu", "eea.europa.eu"):
+            return "production"
+        elif host in ("staging.eea.europa.eu"):
+            return "staging"
+        else:
+            logger.info("Unexpected hostname, default sentry env: devel")
+            return "devel"
+
+    def version(self):
+        """ KGS version
+        """
+        return os.environ.get("EEA_KGS_VERSION", "")
 
     def __call__(self):
-        host = self.request._orig_env.get('HTTP_X_FORWARDED_HOST')
-        if not host:
-            # not behind varnish or no real request yet
-            if '.eea.europa.eu' in self.request._orig_env.get('HTTP_HOST'):
-                return os.environ.get('DEVEL_SENTRY_DSN')
-            else:
-                return None
-        if host in ['www.eea.europa.eu',
-                    'eea.europa.eu']:
-            return os.environ.get('PROD_SENTRY_DSN')
-        elif host in ['staging.eea.europa.eu']:
-            return os.environ.get('STAGING_SENTRY_DSN')
-        else:
-            # This should not happen, but if it does, we want to catch it
-            # so return the devel DSN
-            log.info('Unexpected hostname, default devel DSN used')
-            return os.environ.get('DEVEL_SENTRY_DSN')
+        return os.environ.get("SENTRY_DSN", "")
--- eea/design/skins/eeadesign_js/sentry.js
+++ eea/design/skins/eeadesign_js/sentry.js
@@ -1,7 +1,12 @@
 $(function(){
     var sentry_dsn = $('body').data('sentry-dsn');
+    var sentry_env = $('body').data('sentry-env');
+    var sentry_ver = $('body').data('sentry-ver');
     if (sentry_dsn !== undefined){
         Raven.config(sentry_dsn, {
+            logger: 'javascript',
+            release: sentry_ver,
+            environment: sentry_env,
             ignoreErrors: [
                 'jQuery is not defined',
                  '$ is not defined',
