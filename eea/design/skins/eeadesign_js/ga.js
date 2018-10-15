32ba23e5e0bec34cab64c3d90836092b61083abb
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-10-15 12:34:00 +0300

    Refs #98863 - Set custom dimension DNT

--- eea/design/skins/eeadesign_js/ga.js
+++ eea/design/skins/eeadesign_js/ga.js
@@ -1,22 +1,35 @@
 /* jslint:disable */
 
-/* Google Analytics */
+/*
+* Count DNT-signals
+* https://www.quantable.com/analytics/how-many-do-not-track/
+*/
+var DNT = 'no';
+if (navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.doNotTrack == "yes" || window.doNotTrack == "1" || window.msDoNotTrack == "1"){
+ DNT = 'yes';
+}
+
+
+/* GA */
 if(navigator.userAgent.indexOf("Speed Insights") == -1) {
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    
+
     ga('create', 'UA-184389-1', 'auto', {'legacyCookieDomain': 'eea.europa.eu'});
+    ga('set', 'dimension2', DNT);
     ga('set', 'anonymizeIp', true);
     ga('send', 'pageview');
 }
+
 
 /* Matomo Analytics */
 var _paq = _paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
 _paq.push(["setCookieDomain", "*.eea.europa.eu"]);
+_paq.push(['setCustomDimension', 2, DNT]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
